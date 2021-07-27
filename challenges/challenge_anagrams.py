85c349cf31db925beb04f1d2a249b74faf1c3f49
Serudo <andreserudo@gmail.com>
2021-07-27 17:13:52 -0400

    schedule

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    # print(len(first_string))
+    # for index in range(len(first_string)):
+    #     print(f'index:{index}')
+    #     print(second_string.find(first_string[index]))
+    #     print(first_string[index])
+    #     #if(not second_string.find(first_string[index])):
+    #         #return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,19 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if(target_time is None):
+        return None
+    period_in = 0
+    for index in range(len(permanence_period)):
+        if((permanence_period[index][0] is None) or
+        (permanence_period[index][1] is None)):
+            return None        
+        elif((target_time >= permanence_period[index][0]) and
+            (target_time <= permanence_period[index][1])):
+            period_in += 1
+    return period_in
+
+
+# if (__name__ == '__main__'):
+#     permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+#     target_time1 = 3
+#     print(study_schedule(permanence_period, target_time1))
