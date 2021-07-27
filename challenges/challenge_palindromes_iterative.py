a3e2f699937aec1cf05c5df0d524724ba51d34d5
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-07-27 15:19:01 -0300

    Corrigindo req-1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    word_reverse = ""
+    cont = len(word) - 1
+    for letter in word:
+        word_reverse += word[cont]
+        cont -= 1
+    if word == word_reverse:
+        return True
+    return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    contador = 0
+    count = 0
     if target_time is None:
         return None
     for time in permanence_period:
-        if time[0] is None or time[1] is None:
+        if time[0] is None or time[1] is None or type(time[0]) == str:
             return None
         if target_time >= time[0] and target_time <= time[1]:
-            contador += 1
-    return contador
+            count += 1
+    return count
