a64e70412b957051a12f8233f377d449d4b80abf
MoisesSantana <moisaant@gmail.com>
2021-07-27 19:06:05 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_list = list(first_string)
+    second_list = list(second_string)
+
+    if len(first_list) != len(second_list):
+        return False
+
+    for letter in first_list:
+        try:
+            index = second_list.index(letter)
+        except ValueError:
+            return False
+
+        second_list.remove(second_list[index])
+
+    return True
+
+
+# https://www.w3schools.com/python/ref_string_find.asp
+# https://www.vivaolinux.com.br/topico/Python/Verificar-existencia-de-uma-letra-em-uma-string
+# https://www.geeksforgeeks.org/python-list-index/
