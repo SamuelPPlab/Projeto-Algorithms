2db9d434e999e2b37a25a3fe77c67a70adcf2f36
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-27 16:39:25 -0300

    flake fix

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,15 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     times = 0
-
     if target_time is None or type(target_time) != int:
         return None
-
     for student in permanence_period:
         if type(student[0]) != int or type(student[1]) != int:
             return None
         if student[0] <= target_time <= student[1]:
-            times += 1        
-
-
+            times += 1
     return times
