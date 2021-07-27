36257dcf2073cd05027bacf48d48c24296cd7bd5
nevesfreire <nando.neves@gmail.com>
2021-07-27 16:19:03 -0300

    first

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    # i = 0
+    # if word == '':
+    #     return False
+    # result = True
+    # while i < len(word) // 2 + 1:
+    #     if word[i] != word[len(word) - i - 1]:
+    #         result = False
+    # return result
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        count = 0
+        for student in permanence_period:
+            if student[0] <= target_time and student[1] >= target_time:
+                count += 1
+        return count
+    except TypeError:
+        return None
