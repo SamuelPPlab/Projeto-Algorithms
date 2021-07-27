213002a779dcea229ea6025427e59621f4525129
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-27 18:09:20 -0300

    tirando mais validações do 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,6 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
 
     times = 0
-    if target_time is None or type(target_time) != int:
-        return None
     try:
         for student in permanence_period:
             if student[0] <= target_time <= student[1]:
