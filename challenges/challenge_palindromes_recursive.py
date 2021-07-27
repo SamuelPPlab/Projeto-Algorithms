38ecbf7223b645bc24baf3cc196c84617907459d
Serudo <andreserudo@gmail.com>
2021-07-27 16:09:14 -0400

    palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if(word == ''):
+        return False
+    if(len(word) == 1):
+        return True
+    if(word[0] == word[len(word)-1]):
+        return is_palindrome_recursive(
+            word[1:len(word)-1],
+            low_index,
+            high_index
+        )
+    else:
+        return False
