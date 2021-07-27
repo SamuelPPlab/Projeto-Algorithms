5bb914daca72c77e1dd0cee273b63692fd8ec538
Bruno <bruno_grb@hotmail.com>
2021-07-27 15:17:58 -0300

    challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    "INICIO PROJETO"
+    if (type(target_time) != int):
+        return None
+    counter = 0
+    for schedule in permanence_period:
+        if (type(schedule[0]) != int or type(schedule[1]) != int):
+            return None
+        if (schedule[0] <= target_time and schedule[1] >= target_time):
+            counter += 1
+    return counter
