3980864ef27394a778ad636a37773c626f097405
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-27 19:18:48 -0300

    Req 02 - 27/07/2021

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,38 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # if len(word) < 2:
+    #     return True
+    # elif word[0] != word[-1]:
+    #     return False
+    # else:
+    #     return is_palindrome_recursive(word[1:-1], low_index, high_index)
+    try:
+        if low_index == high_index:
+            return True
+        elif word[low_index] != word[high_index]:
+            return False
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except IndexError:
+        return False
+
+
+if __name__ == "__main__":
+    word = "ANA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    # saída: True
+
+    word = "SOCOS"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    # saída: True
+
+    word = "REVIVER"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    # saída: True
+
+    word = "COXINHA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    # saída: False
+
+    word = "AGUA"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
+    # saída: False
