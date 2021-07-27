02a1bee55292d025fef4ede5ebf29c56e83493fc
nevesfreire <nando.neves@gmail.com>
2021-07-27 16:54:20 -0300

    second

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,10 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
-    # i = 0
-    # if word == '':
-    #     return False
-    # result = True
-    # while i < len(word) // 2 + 1:
-    #     if word[i] != word[len(word) - i - 1]:
-    #         result = False
-    # return result
+    if word == '':
+        return False
+    low = low_index or 0
+    high = high_index or len(word) - 1
+    if word[low] != word[high]:
+        return False
+    if low == len(word) - 1:
+        return True
+    return is_palindrome_recursive(word, low + 1, high - 1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
     try:
         count = 0
         for student in permanence_period:
-            if student[0] <= target_time and student[1] >= target_time:
+            if student[0] <= target_time <= student[1]:
                 count += 1
         return count
     except TypeError:
