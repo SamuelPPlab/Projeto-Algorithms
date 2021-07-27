c7141cd0cce526604d7f26eaf503c31ddfb77e2f
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2021-07-27 17:16:43 -0300

    Iniciando o projeto

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
     times = 0
-    if target_time is None or type(target_time) != int:
+    if target_time is None or type(target_time) is not int:
         return None
     for student in permanence_period:
-        for teste in student:
-            if type(teste) != int:
-                return None
+        if type(student[0]) is not int or type(student[1]) is not int:
+            return None
         if student[0] <= target_time <= student[1]:
             times += 1
 
