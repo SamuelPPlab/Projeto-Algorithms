baead3e0e0cf18165b805c5b77ce7e4afdb70917
CleberFontinele <cleberfontinele2387@gmail.com>
2021-07-27 16:56:41 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+
+    result = 0
+    if target_time is None:
+        return None
+    for i in permanence_period:
+        if not isinstance(i[0], int) or not isinstance(i[1], int):
+            return None
+        elif target_time >= i[0] and target_time <= i[1]:
+            result += 1
+    return result
