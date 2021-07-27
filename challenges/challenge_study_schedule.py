4a3918619d4165e1fe533619060c7d3f2c5303b8
MoisesSantana <moisaant@gmail.com>
2021-07-27 16:20:06 -0300

    correção flake 8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == None:
+    if target_time is None:
         return None
 
     count = 0
     for student_period in permanence_period:
-        if student_period[0] == None or student_period[1] == None:
+        if student_period[0] is None or student_period[1] is None:
             return None
 
         if student_period[0] <= target_time <= student_period[1]:
