ce588cc59a39912e4f0ce507ba283650fb641087
Vitor Rodrigues <vitor_rc@outlook.com>
2021-07-27 15:12:51 -0300

    ajuste find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,14 +1,15 @@
 def find_duplicate(nums):
-    if len(nums) <= 1:
+    if len(nums) <= 1 or any(type(num) is str or num < 0 for num in nums):
         return False
 
     count_dict = {}
     for num in nums:
-        if type(num) is str or num < 0:
-            return False
-        elif num in count_dict:
+        if num in count_dict:
             count_dict[num] += 1
         else:
             count_dict[num] = 1
+
+    if all(value == 1 for value in count_dict.values()):
+        return False
 
     return max(count_dict, key=count_dict.get)
