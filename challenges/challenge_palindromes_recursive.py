72e32f15043b821700cd5039b029d656f861750e
Melissa Gomes <mel.ge171201@gmail.com>
2021-07-27 16:13:42 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) <= 0:
+        return False
+    elif word[0] != word[-1]:
+        return False
+    elif len(word) < 2:
+        return True
+    else:
+        return is_palindrome_recursive(word[1:-1], 0, len(word) -1)
