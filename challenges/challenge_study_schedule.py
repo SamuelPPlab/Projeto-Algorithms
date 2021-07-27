4612281c1733dcb7cc1958368b4dc9adc0cba10e
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-07-27 19:52:21 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    try:
+        counter = 0
+        for time in permanence_period:
+            if time[0] <= target_time and time[1] >= target_time:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
