9057a32112b196c4e2dd21ea36d5aa44e8f4f50c
Serudo <andreserudo@gmail.com>
2021-07-27 17:54:47 -0400

    anagrams - second part

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -5,14 +5,9 @@ def is_anagram(first_string, second_string):
     if(len(first_string) != len(second_string)):
         return False
     for index in range(len(first_string)):
-        # print(first_string[index])
-        # print(second_string.find(first_string[index]))
-        if(second_string.find(first_string[index]) < 0):
+        substring = first_string[index]     
+        count_in_first = first_string.count(substring)
+        count_in_second = second_string.count(substring)        
+        if(count_in_first != count_in_second):
             return False
     return True
-
-
-# if (__name__ == '__main__'):
-#     first_string = "coxinha"
-#     second_string = 'empada'
-#     print(is_anagram(first_string, second_string))
