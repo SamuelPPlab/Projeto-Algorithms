d6a83ba64288bab6c217184c31541a5c62d42b9d
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-07-27 19:24:47 -0300

    Refatoração Anagrama - Algoritmo de ordenação

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,25 +2,18 @@ def is_anagram(first_string, second_string):
     if not first_string or not second_string:
         return False
 
-    return "".join(insertionSort(list(first_string))) == "".join(
-        insertionSort(list(second_string))
-    )
+    return sortString(first_string) == sortString(second_string)
 
 
-# Esolha Insertion Sort: Complexidade O(n^2)
-# Referência: https://www.geeksforgeeks.org/insertion-sort/
-# Python program for implementation of Insertion Sort
-# Function to do insertion sort
-def insertionSort(arr):
-    # Traverse through 1 to len(arr)
-    for i in range(1, len(arr)):
-        key = arr[i]
-        # Move elements of arr[0..i-1], that are
-        # greater than key, to one position ahead
-        # of their current position
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
+def sortString(str):
+    SHIFT = 32
+    MAX_CHAR = 90
+    result = ""
+    charCount = [0 for i in range(MAX_CHAR)]
+    for i in range(0, len(str), 1):
+        charCount[ord(str[i]) - SHIFT] += 1
+    for i in range(0, MAX_CHAR, 1):
+        for j in range(0, charCount[i], 1):
+            result += chr(SHIFT + i)
+
+    return result
