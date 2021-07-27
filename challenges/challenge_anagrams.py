20378cbaeb02854dbc863b5f5485c8023b835558
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-27 18:01:55 -0300

    feat: anagram method using counter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
+from collections import Counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # if first_string[::-1] != second_string:
+    if Counter(first_string) != Counter(second_string):
+        return False
+    else:
+        return True
