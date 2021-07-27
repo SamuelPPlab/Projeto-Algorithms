a9f013f0d13c1a5d68e16d6d3f82d2a3f5b3af56
MoisesSantana <moisaant@gmail.com>
2021-07-27 16:13:43 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None:
+        return None
+
+    count = 0
+    for student_period in permanence_period:
+        if student_period[0] == None or student_period[1] == None:
+            return None
+
+        if student_period[0] <= target_time <= student_period[1]:
+            count += 1
+
+    return count
