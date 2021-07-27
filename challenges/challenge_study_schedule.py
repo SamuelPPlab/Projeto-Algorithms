3376de06a252f1ce20c87ae4d0058aca1c82c5f2
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-07-27 17:57:26 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    sum = 0
+    
+    if target_time is None or type(target_time) != int:
+        return None
+    
+    for item in permanence_period:
+        if type(item[0]) != int or type(item[1]) != int:
+            return None
+        if item[0] <= target_time and target_time <= item[1]:
+            sum += 1
+    return sum
+
