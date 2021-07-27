c56987e78e4a0745f240a5f4b6e7aee7b90b0c6e
renatomak <renato.mark.silva@gmail.com>
2021-07-27 19:32:43 -0300

    RQ1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    number_students_in_interval = 0
+
+    for start, end in permanence_period:
+        if not (isinstance(start, int) and isinstance(end, int) and isinstance(target_time, int)):
+            return None
+
+        if start <= target_time <= end:
+            number_students_in_interval += 1
+
+    return number_students_in_interval
+
+
+# if __name__ == '__main__':
+#     permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+#     result = study_schedule(permanence_period, 4)
