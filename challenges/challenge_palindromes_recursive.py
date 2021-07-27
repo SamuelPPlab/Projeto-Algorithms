556be229f124573d282fcdf53cef7a87dc3f2f77
julioKauer <juliokauer@gmail.com>
2021-07-27 19:31:40 -0300

    Flake error

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,4 +3,7 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if low_index >= high_index:
         return True
-    return word[low_index] == word[high_index] and is_palindrome_recursive(word, low_index + 1, high_index -1)
+    return (
+        word[low_index] == word[high_index] and
+        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        )
