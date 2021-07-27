4791c33987104868e9acbd20d73f15bb8add5f5e
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-27 19:17:04 -0300

    refactor: anagram refactor

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,8 +2,4 @@ from collections import Counter
 
 
 def is_anagram(first_string, second_string):
-    # if first_string[::-1] != second_string:
-    if Counter(first_string) != Counter(second_string):
-        return False
-    else:
-        return True
+    return Counter(first_string) == Counter(second_string)
