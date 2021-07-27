21eb6253bff9d52a4f180dc79512310e85d69017
moacyrrln <moacyrrln@gmail.com>
2021-07-27 17:59:40 -0300

    requisito2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -9,3 +9,19 @@ def reverse(list):
         return list
     else:
         return reverse(list[1:]) + list[0]
+
+
+# def is_palindrome_recursive(word, low_index, high_index):
+#     if len(word) < 2 and word != "":
+#         return True
+#     if word == '':
+#         return False
+#     else:
+#         if word[low_index] == word[high_index]:
+#             word = word[1:-1]
+#             return is_palindrome_recursive(word, low_index, len(word) - 1)
+#         else:
+#             return False
+
+
+# print(is_palindrome_recursive("", 0, 0))
