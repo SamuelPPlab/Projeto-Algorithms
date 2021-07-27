03a4d5c17131f7ad9d9c7987bce9dd69efee85a5
kramer <kramerscs@gmail.com>
2021-07-27 15:19:41 -0300

    feat: req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        lower = period[0]
+        upper = period[1]
+        if type(lower) != type(upper) != 'int':
+            return None
+        elif target_time >= lower and target_time <= upper:
+            counter += 1
+    return counter
