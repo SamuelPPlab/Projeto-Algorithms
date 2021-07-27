2488c047656fdc10ca64de03ac56fad9dd85e070
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-27 19:07:03 -0300

    feat: find the duplicate method

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    for (index, number) in enumerate(nums[:-1]):
+        if not isinstance(number, int) or number < 0:
+            return False
+        if number == nums[index + 1]:
+            return number
+    return False
+
+
+nu = [1, 3, 4, 2]
+print(find_duplicate(nu))
