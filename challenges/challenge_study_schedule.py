75c019e608a5c85ddac3c1a6b42c84ef45312f2b
Vitor Rodrigues <vitor_rc@outlook.com>
2021-07-27 15:29:36 -0300

    study_schedule ajuste

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,16 @@
 def study_schedule(permanence_period, target_time):
     cont = 0
 
+    if (
+        target_time is None
+    ):
+        return None
+
     for index in range(len(permanence_period)):
         if (
             not isinstance(permanence_period[index][0], int) or
-            not isinstance(permanence_period[index][1], int) or
-            target_time is None
+            not isinstance(permanence_period[index][1], int)
+            # not all(isinstance(value, int) for value in permanence_period[index]) or
         ):
             return None
 
