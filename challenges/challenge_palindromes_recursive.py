adbcca25280250dba4c33342ea82a124d64686c7
Pablo Pessanha <pablopessanh@gmail.com>
2021-07-27 19:22:45 -0300

    requisitos obrigatorios feitos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low, high):
+    if word == "" or word[low] != word[high]:
+        return False
+    if low == high:
+        return True
+
+    high_index = high - 1
+    low_index = low + 1
+    return is_palindrome_recursive(word, low_index, high_index)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None: return None
+    count = 0
+    for start, end in permanence_period:
+        if start is None or end is None: return None
+        if target_time >= start and target_time <= end:
+            count += 1
+    return count
