3ac2174f23d6e41d0f292494c4570dbac70b20f1
moacyrrln <moacyrrln@gmail.com>
2021-07-27 16:36:57 -0300

    requisito1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    if not start_time or not target_time:
-        return 0
+    if not permanence_period or not target_time:
+        if target_time != 0:
+            return None
     quantity = 0
-    for item in range(len(start_time)):
-        if start_time[item] <= target_time <= end_time[item]:
-            quantity += 1
+
+    for item in permanence_period:
+        if not (type(item[0]) == int) or not (type(item[1]) == int):
+            return None
+        else:
+            if item[0] <= target_time <= item[1]:
+                quantity += 1
+
     return quantity
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+
+print(study_schedule(permanence_period, 3))
