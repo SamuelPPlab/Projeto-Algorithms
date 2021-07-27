99d7631d65f57c4826e9f4785c690f28279ce8f2
Vitor Rodrigues <vitor_rc@outlook.com>
2021-07-27 17:19:06 -0300

    palindrome_iterative ajuste

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,10 +2,7 @@ def is_palindrome_iterative(word):
     if not word:
         return False
     status = True
-    midlle = len(word) // 2
-    for index in range(len(word)):
-        if index == midlle:
-            break
+    for index in range(len(word) // 2):
         if word[index] != word[len(word) - 1 - index]:
             status = False
 
