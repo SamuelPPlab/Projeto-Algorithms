c06161604edba3d96c1f765190855f8d34470207
Melissa Gomes <mel.ge171201@gmail.com>
2021-07-27 15:42:17 -0300

    testando o req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    count = 0
+
+    if target_time == None or type(target_time) != int : 
+        return None
+
+    for aluno in permanence_period :
+        if type(aluno[0]) != int or type(aluno[1]) != int :
+            return None
+        if aluno[0] <= target_time <= aluno[1] :
+            count += 1
+
+
+    return count
