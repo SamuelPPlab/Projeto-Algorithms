178be6ff63b02560319ddd5c7b37a0f3ee99d265
moacyrrln <moacyrrln@gmail.com>
2021-07-27 18:33:36 -0300

    requisito3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,14 @@
 def is_anagram(first_string, second_string):
-    return ''.join(sorted(list(first_string))) == \
-      ''.join(sorted(list(second_string)))
+    return ''.join(bubbleSort(list(first_string))) == \
+      ''.join(bubbleSort(list(second_string)))
+
+
+def bubbleSort(alist):
+    alist = list(alist)
+    for passnum in range(len(alist)-1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return ''.join(alist)
