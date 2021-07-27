3ea5ea730bd17b0bedeb1927135a93e5cfdcce5a
Luise Rios <luisemiranda@gmail.com>
2021-07-27 18:07:44 -0300

    corrigindo diferenças req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,15 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        array[minimum], array[i] = array[i], array[minimum]
+    return array
+
+
 def is_anagram(first_string, second_string):
-    first_list = list(first_string)
-    second_list = list(second_string)
-    first_list.sort()
-    second_list.sort()
+    first_list = selection_sort(list(first_string))
+    second_list = selection_sort(list(second_string))
+
     return first_list == second_list
