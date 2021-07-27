06ffc89fa40e3ebe43dc571d418ff1b3872f1574
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-07-27 18:29:41 -0300

    mais uma vez

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,14 @@
 def study_schedule(permanence_period, target_time):
     try:
-        total_list = []
+        counter = 0
         for period in permanence_period:
-            if target_time >= 0:
-                total_list += list(range(period[0], period[1] + 1))
+            if target_time >= 0 and (
+                target_time in range(period[0], period[1] + 1)
+            ):
+                counter += 1
     except TypeError:
         return None
-    return total_list.count(target_time)
+    return counter
 # referencia:
 # https://pt.stackoverflow.com/questions/341085/como-validar-se-um-valor-%C3%A9-uma-tupla-possuindo-uma-string-e-um-inteiro
 # try/except https://github.com/tryber/sd-07-project-algorithms/pull/3/files
