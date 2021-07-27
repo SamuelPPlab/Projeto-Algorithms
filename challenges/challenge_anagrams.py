2fdbee595bc2b05046e003f249d2814e35e1de5e
Serudo <andreserudo@gmail.com>
2021-07-27 17:55:18 -0400

    anagrams - fix lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -5,9 +5,9 @@ def is_anagram(first_string, second_string):
     if(len(first_string) != len(second_string)):
         return False
     for index in range(len(first_string)):
-        substring = first_string[index]     
+        substring = first_string[index]
         count_in_first = first_string.count(substring)
-        count_in_second = second_string.count(substring)        
+        count_in_second = second_string.count(substring)
         if(count_in_first != count_in_second):
             return False
     return True
