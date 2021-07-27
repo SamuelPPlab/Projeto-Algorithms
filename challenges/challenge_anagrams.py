3cdd90054526ab8ac5e72af16136599f03567ecd
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-07-27 17:53:43 -0300

    req-3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui"""
+    count = 0
+    for letter in first_string:
+        for letter_second in second_string:
+            if letter_second == letter:
+                count += 1
+    if (
+        count == len(first_string)
+        and first_string != ""
+        and second_string != ""
+    ):
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) < 2:
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(
+            word[1:-1], low_index, len(word[1:-1]) - 1
+        )
+    return False
