1f489b30087f0e9a2102c2b5af40003fd83b9160
Melissa Gomes <mel.ge171201@gmail.com>
2021-07-27 15:46:45 -0300

    sem erros de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,14 +2,13 @@ def study_schedule(permanence_period, target_time):
 
     count = 0
 
-    if target_time == None or type(target_time) != int : 
+    if target_time is None or type(target_time) != int:
         return None
 
-    for aluno in permanence_period :
-        if type(aluno[0]) != int or type(aluno[1]) != int :
+    for aluno in permanence_period:
+        if type(aluno[0]) != int or type(aluno[1]) != int:
             return None
-        if aluno[0] <= target_time <= aluno[1] :
+        if aluno[0] <= target_time <= aluno[1]:
             count += 1
-
 
     return count
