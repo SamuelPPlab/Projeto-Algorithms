d8ea79999696a52319c3e68904d9ffaa61caa5ff
julioKauer <juliokauer@gmail.com>
2021-07-27 18:57:40 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
+    online_students = 0
+    for initial, final in permanence_period:
+        if type(initial) != int or type(final) != int:
+            return None
+        if initial <= target_time <= final:
+            online_students += 1
+    return online_students
