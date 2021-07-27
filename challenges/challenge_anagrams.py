0d35c22d8fbd850b7926e533f89c2430dbd5b6ca
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-27 19:04:29 -0300

    Requisito 3 - Algoritimo de melhoria de tempo para testar no avaliador

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,18 +1,57 @@
 def sort_list(lista):
-    has_swapped = True
-    num_of_iterations = 0
-    while has_swapped:
-        has_swapped = False
+    if len(lista) <= 1:
+        return lista
 
-        for index in range(len(lista) - num_of_iterations - 1):
-            if lista[index] > lista[index + 1]:
-                lista[index], lista[index + 1] = (
-                    lista[index + 1],
-                    lista[index],
-                )
-                has_swapped = True
-        num_of_iterations += 1
-    return lista
+    mid = len(lista) // 2
+    left, right = sort_list(lista[:mid]), sort_list(lista[mid:])
+    return merge(left, right, lista.copy())
+
+
+def merge(left, rigth, merged):
+    left_cursor, rigth_cursor = 0, 0
+
+    while left_cursor < len(left) and rigth_cursor < len(rigth):
+        if left[left_cursor] <= rigth[rigth_cursor]:
+            merged[left_cursor + rigth_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + rigth_cursor] = rigth[rigth_cursor]
+            rigth_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + rigth_cursor] = left[left_cursor]
+
+    for rigth_cursor in range(rigth_cursor, len(rigth)):
+        merged[left_cursor + rigth_cursor] = rigth[rigth_cursor]
+
+    return merged
+
+# -------------------------------------------------------------------
+# for i in range(len(lista)):
+#     minimum = i
+#     for j in range(i + 1, len(lista)):
+#         if lista[j] < lista[minimum]:
+#             minimum = j
+#     lista[minimum], lista[i] = lista[i], lista[minimum]
+
+# return lista
+
+# -------------------------------------------------------------------
+
+# has_swapped = True
+# num_of_iterations = 0
+# while has_swapped:
+#     has_swapped = False
+
+#     for index in range(len(lista) - num_of_iterations - 1):
+#         if lista[index] > lista[index + 1]:
+#             lista[index], lista[index + 1] = (
+#                 lista[index + 1],
+#                 lista[index],
+#             )
+#             has_swapped = True
+#     num_of_iterations += 1
+# return lista
 
 
 def is_anagram(first_string, second_string):
@@ -37,6 +76,6 @@ def is_anagram(first_string, second_string):
     # return list_first_string, list_second_string
 
 
-# first_string = "FERNANDO"
-# second_string = "FERNANDA"
+# first_string = "ROMA"
+# second_string = "AMOR"
 # print(is_anagram(first_string, second_string))
