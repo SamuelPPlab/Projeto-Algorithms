f03d6744ed483962867b30cb7174d2f32b17c77f
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-07-27 17:48:19 -0300

    reratorando para ser mais rapido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,32 +1,14 @@
-from collections import Counter
-
-
-def verifica(period):
-    return isinstance(period[0], int) and isinstance(period[1], int)
-
-
-def total_list_range(permanence_period):
-    total_list = []
-    for tuple_time in permanence_period:
-        total_list += list(range(tuple_time[0], tuple_time[1] + 1))
-    return total_list
-
-
 def study_schedule(permanence_period, target_time):
-    todas_tuplas_validas = True
-    if not isinstance(target_time, int):
+    try:
+        total_list = []
+        if not isinstance(target_time, int):
+            return None
+
+        for period in permanence_period:
+            total_list += list(range(period[0], period[1] + 1))
+    except TypeError:
         return None
-
-    for period in permanence_period:
-        if not verifica(period):
-            todas_tuplas_validas = False
-            break
-
-    if not todas_tuplas_validas:
-        return None
- 
-    total_list = total_list_range(permanence_period)
-    best_period = Counter(total_list)
-    return best_period[target_time]
-
+    return total_list.count(target_time)
+# referencia:
 # https://pt.stackoverflow.com/questions/341085/como-validar-se-um-valor-%C3%A9-uma-tupla-possuindo-uma-string-e-um-inteiro
+# try/except https://github.com/tryber/sd-07-project-algorithms/pull/3/files
