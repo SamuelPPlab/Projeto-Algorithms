0003d56435bf78758b9a5982ce918111e92d007e
Vanessa Oliveira <vanessanaara@gmail.com>
2021-07-27 17:02:17 -0300

    Requsito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for timeInterval in permanence_period:
+        entry, exit = timeInterval
+        if type(entry) is not int or type(exit) is not int:
+            return None
+        if timeInterval[0] <= target_time <= timeInterval[1]:
+            counter += 1
+    return counter
