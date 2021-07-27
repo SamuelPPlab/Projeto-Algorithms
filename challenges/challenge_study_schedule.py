f68cc43783743aa705e03b67e9fcdbbd1ce73187
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-07-27 15:08:56 -0300

    Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    START = 0
+    END = 1
+
+    if target_time is None:
+        return None
+
+    for _index, period in enumerate(permanence_period):
+        if not isinstance(period[START], int) or not isinstance(
+            period[END], int
+        ):
+            return None
+        if period[START] <= target_time <= period[END]:
+            counter += 1
+
+    return counter
