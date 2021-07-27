58a0f78b404cf62d6bac23130d4d46dc574f63c7
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-07-27 17:45:53 -0300

    Requisito 05

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    for i in range(0, int(len(word) / 2)):
+        if word[i] != word[len(word) - i - 1]:
+            return False
+    return True
