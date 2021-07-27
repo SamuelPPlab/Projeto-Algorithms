b6f545c16cc4e6932321771fb0792bdb4780d0c5
moacyrrln <moacyrrln@gmail.com>
2021-07-27 18:57:40 -0300

    achar o duplicado2

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,15 @@
-def find_duplicate(nums):
+def valid(nums):
     if nums == []:
         return False
     order = sorted(nums)
     if type(order[0]) == str or order[0] < 0:
         return False
+
+
+def find_duplicate(nums):
+    if valid(nums) is False:
+        return False
+    order = sorted(nums)
     for item in range(len(order)-1):
         if type(order[item]) == str:
             return False
