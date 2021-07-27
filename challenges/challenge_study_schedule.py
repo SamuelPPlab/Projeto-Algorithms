4bd50d32dc8df8dc3f94e7209a405b3ba70d61e0
danwhat <daanwhat@gmail.com>
2021-07-27 15:34:00 -0300

    req 1 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    total_time = 0
+    for aluno in permanence_period:
+        try:
+            if target_time >= aluno[0] and target_time <= aluno[1]: total_time = total_time + 1
+        except: return None
+    return total_time
+
+# print(study_schedule([(2, 2), (1, 2), ("batata", 3), (1, 5), (4, 5), (4, 5)],3))
