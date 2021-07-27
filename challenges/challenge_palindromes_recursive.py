d1b30795286658df072688c8d5672302a98c987d
MoisesSantana <moisaant@gmail.com>
2021-07-27 17:36:07 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,20 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    print('oi')
+    if (not word) or (word[low_index] != word[high_index]): return False
+    
+    if high_index == 1: return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+'''
+    [a]rar[a] - 0 - 4
+    a[r]a[r]a - 1 - 3
+    ar[[a]]ra - 2 - 2
+    arara - 3 - 1 (True - linha 4)
+
+    [m]aç[ã] - 0 - 3 (False - linha 2)
+
+    [a]bbacb[a] - 0 - 6
+    a[b]bac[b]a - 1 - 5
+    ab[b]a[c]ba - 2 - 4 (False - linha 2)
+'''
