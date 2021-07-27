d60b82f4278f71abfa0247b26af838532e548757
Serudo <andreserudo@gmail.com>
2021-07-27 17:18:22 -0400

    schedule - fix lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,10 +5,10 @@ def study_schedule(permanence_period, target_time):
     period_in = 0
     for index in range(len(permanence_period)):
         if((permanence_period[index][0] is None) or
-        (permanence_period[index][1] is None)):
-            return None        
+           (permanence_period[index][1] is None)):
+            return None
         elif((target_time >= permanence_period[index][0]) and
-            (target_time <= permanence_period[index][1])):
+             (target_time <= permanence_period[index][1])):
             period_in += 1
     return period_in
 
