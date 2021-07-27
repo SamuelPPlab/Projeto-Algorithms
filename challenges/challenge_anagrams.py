50b5aa464d00f2e27021fb61e98f8a7cb5542d3d
Vanessa Oliveira <vanessanaara@gmail.com>
2021-07-27 18:50:14 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    for letter1 in first_string:
+        for letter2 in second_string:
+            if letter1 == letter2:
+                return True
+            else:
+                return False
