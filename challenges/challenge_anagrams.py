ccdb78e6dd2b4c4ce5e180d4de8e95514be3554c
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-27 18:19:56 -0300

    Requisito 3 - correções de lint - ainda não passando no teste de tempo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,41 +1,42 @@
+def sort_list(lista):
+    has_swapped = True
+    num_of_iterations = 0
+    while has_swapped:
+        has_swapped = False
+
+        for index in range(len(lista) - num_of_iterations - 1):
+            if lista[index] > lista[index + 1]:
+                lista[index], lista[index + 1] = (
+                    lista[index + 1],
+                    lista[index],
+                )
+                has_swapped = True
+        num_of_iterations += 1
+    return lista
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    if first_string == "" or second_string == "" or len(first_string) != len(second_string):
+    if (
+        first_string == ""
+        or second_string == ""
+        or len(first_string) != len(second_string)
+    ):
         return False
 
     list_first_string = [char for char in first_string]
     list_second_string = [char for char in second_string]
 
-    has_swapped = True
-    num_of_iterations = 0
-    while has_swapped:
-        has_swapped = False
+    list_first_string = sort_list(list_first_string)
+    list_second_string = sort_list(list_second_string)
 
-        for index in range(len(list_first_string) - num_of_iterations - 1):
-            if list_first_string[index] > list_first_string[index + 1]:
-                list_first_string[index], list_first_string[index + 1] = list_first_string[index + 1], list_first_string[index]
-                has_swapped = True
-        num_of_iterations += 1
-
-    has_swapped = True
-    num_of_iterations = 0
-    while has_swapped:
-        has_swapped = False
-
-        for index in range(len(list_second_string) - num_of_iterations - 1):
-            if list_second_string[index] > list_second_string[index + 1]:
-                list_second_string[index], list_second_string[index + 1] = list_second_string[index + 1], list_second_string[index]
-                has_swapped = True
-        num_of_iterations += 1
-    
     if list_first_string == list_second_string:
         return True
-    
+
     return False
+    # return list_first_string, list_second_string
 
-    
 
-
-first_string = 'FERNANDO'
-second_string = 'FERNANDA'
-print(is_anagram(first_string, second_string))
+# first_string = "FERNANDO"
+# second_string = "FERNANDA"
+# print(is_anagram(first_string, second_string))
