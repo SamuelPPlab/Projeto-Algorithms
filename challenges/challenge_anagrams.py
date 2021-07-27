3e3c332be2cadc49bcddf4adeb72ea5dd85936c3
Bruno <bruno_grb@hotmail.com>
2021-07-27 16:19:28 -0300

    iniciando challenge anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (
+        len(first_string) == 0
+        or len(second_string) == 0
+        or len(first_string) != len(second_string)
+    ):
+        return False
+    first_string_sorted = sorted(first_string)
+    second_string_sorted = sorted(second_string)
+    if (first_string_sorted == second_string_sorted):
+        return True
+    return False
