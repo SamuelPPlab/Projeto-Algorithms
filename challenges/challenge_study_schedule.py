8b11346f11258ca48ad69fd4a3173d7ce3e2e43f
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-07-27 15:04:14 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    contador = 0
+    if target_time is None:
+        return None
+    for time in permanence_period:
+        if time[0] is None or time[1] is None:
+            return None
+        if target_time >= time[0] and target_time <= time[1]:
+            contador += 1
+    return contador
