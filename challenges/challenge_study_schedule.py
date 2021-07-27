987177efdadc7cd65debed6bdf9d0f5f6c954ea5
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-27 17:02:31 -0300

    feat: most frequent schedule calculation

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counter = 0
+        for p in permanence_period:
+            if target_time >= p[0] and target_time <= p[1]:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
