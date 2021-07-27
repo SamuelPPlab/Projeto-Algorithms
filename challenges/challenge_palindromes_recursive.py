7c1e018cf2a7d5b505652d84fce9e057e422fb84
julioKauer <juliokauer@gmail.com>
2021-07-27 19:26:49 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if low_index >= high_index:
+        return True
+    return word[low_index] == word[high_index] and is_palindrome_recursive(word, low_index + 1, high_index -1)
