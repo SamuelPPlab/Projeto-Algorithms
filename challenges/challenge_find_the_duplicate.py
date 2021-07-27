3c532643d42ca99b049cf872bf1125286051b020
danwhat <daanwhat@gmail.com>
2021-07-27 16:53:43 -0300

    Agora vai 2

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,6 @@
 def find_duplicate(nums):
     for num in nums:
-        if  type(num) != int or num < 0:
+        if type(num) != int or num < 0:
             return False
         if (nums.count(num) != 1):
             return num
