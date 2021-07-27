17134321b06e4d216722c06ed8e881485bf24d21
Pablo Pessanha <pablopessanh@gmail.com>
2021-07-27 19:26:02 -0300

    fixes flake8

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,5 @@
 from collections import Counter
 
+
 def is_anagram(first_string, second_string):
     return Counter(first_string) == Counter(second_string)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    if target_time == None: return None
+    if target_time is None:
+        return None
     count = 0
     for start, end in permanence_period:
-        if start is None or end is None: return None
+        if start is None or end is None:
+            return None
         if target_time >= start and target_time <= end:
             count += 1
     return count
