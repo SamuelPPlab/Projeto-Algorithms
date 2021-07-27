41c589e4b6beff6a485c97f393ab1a04396116b1
Filipi Firmino <contatoic3web@gmail.com>
2021-07-27 15:48:45 -0300

    Fix lint error

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,5 @@
 def is_palindrome_iterative(word):
-    if word != "" and word == word[::-1] :            
+    if word != "" and word == word[::-1]:
         return True
     else:
         return False
