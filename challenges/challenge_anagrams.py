414d704575f782cd55963020a9402026304ed703
danwhat <daanwhat@gmail.com>
2021-07-27 17:59:19 -0300

    pretty

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,14 +1,16 @@
 def removeCharacters(string, characters):
-  retorno = list(string)
-  for letra in characters:
+    retorno = list(string)
+    for letra in characters:
         try:
             retorno.remove(letra)
         except Exception:
             return None
-  return "".join(retorno)
+    return "".join(retorno)
+
 
 def is_anagram(first_string, second_string):
     string_final = removeCharacters(first_string, second_string)
     print(string_final)
-    if string_final != None and len(string_final) == 0: return True
+    if string_final != None and len(string_final) == 0:
+        return True
     return False
