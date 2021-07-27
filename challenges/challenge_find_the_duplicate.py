39aa6dc34602b4db91b632e28a71c1315bfe7ae3
danwhat <daanwhat@gmail.com>
2021-07-27 16:13:10 -0300

    req 4 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+  for num in nums:
+    try:
+        if num < 0 : return False
+        if ( nums.count(num) != 1): return num
+    except: return False
+  return False
+if __name__ == '__main__':
+  print(find_duplicate([1, 3, 4, 2, 2]))
