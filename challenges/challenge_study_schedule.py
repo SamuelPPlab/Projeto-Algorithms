18ce8950cd868ebab0e537f46a88c8efdcb9fe15
dennerpl <denner.souzapl@gmail.com>
2021-07-27 18:57:19 -0300

    iniciando projeto algorithms

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+    try:
+        for period in permanence_period:
+            if (period[0] <= target_time <= period[1]):
+                result += 1
+    except TypeError:
+        return None
+    return result
