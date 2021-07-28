0358fc0667dcda84c4abf3027aa9ac69e391a16c
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-28 17:08:31 -0300

    Req 03 - 28/07/2021

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,37 +1,63 @@
-def is_anagram(first_string, second_string):
-    word = ""
-    if len(first_string) != len(second_string):
-        return False
-    for l1 in first_string:
-        if l1 in word:
-            return False
-        elif l1 in second_string:
-            word += l1
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
         else:
-            return False
-    return True
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def is_anagram(first_string, second_string):
+    list_first_word, list_second_word = list(first_string), list(second_string)
+
+    list_sort_second_word = merge_sort(list_second_word)
+    sort_second_word = "".join(list_sort_second_word)
+
+    list_sort_first_word = merge_sort(list_first_word)
+    sort_first_word = "".join(list_sort_first_word)
+
+    if sort_first_word == sort_second_word:
+        return True
+    return False
 
 
 if __name__ == "__main__":
+    # print(merge_sort([100, 4, 6, 33, 56, 67]))
+
     first_string = "amor"
     second_string = "roma"
     print(is_anagram(first_string, second_string))
-    # saída: True
 
     first_string = "pedra"
     second_string = "perda"
     print(is_anagram(first_string, second_string))
-    # saída: True
 
     first_string = "pato"
     second_string = "tapo"
     print(is_anagram(first_string, second_string))
-    # saída: True
 
     first_string = "coxinha"
     second_string = "empada"
     print(is_anagram(first_string, second_string))
-    # saída: False
 
     first_string = "pedra"
     second_string = "pedro"
