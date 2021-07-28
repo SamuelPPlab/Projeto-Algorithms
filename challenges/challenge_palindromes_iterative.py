ad9204c0e81f3622eb70dc3c2dca7f1efb60bb4e
nevesfreire <nando.neves@gmail.com>
2021-07-27 23:04:32 -0300

    teste

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,8 +3,6 @@ def is_palindrome_iterative(word):
     i = 0
     if word == '':
         return False
-    
-
     while i < len(word) // 2:
         if word[i] != word[len(word) - i - 1]:
             return False
