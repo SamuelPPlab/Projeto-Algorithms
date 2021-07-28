ca890433d0decb9b42e13caa2b526dc006a77c8f
Francisco-LT <franciscoliratorres@gmail.com>
2021-07-28 11:16:29 -0300

    add req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # https://www.youtube.com/watch?v=92UyahoAne4
+    if len(word) < 1:
+        return False
+    if len(word) == 1:
+        return True
+    else:
+        return word[low_index] == word[-1] and is_palindrome_recursive(
+            word[1:-1], low_index, (len(word) - 1)
+        )
