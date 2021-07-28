c75f35cc6ed00de5145dc47236cff803358a9907
CarolSi-hub <a.carolg1@gmail.com>
2021-07-28 20:10:26 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    counter = 0
+    for index in range(len(permanence_period)):
+        p_period = permanence_period[index]
+        if None in p_period:
+            return None
+        inicial_hour = p_period[0]
+        end_hour = p_period[1]        
+        if (inicial_hour <= target_time <= end_hour):
+            counter += 1
+    return counter
