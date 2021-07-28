21842ad8e59e00db76491ef209da05438ea3f5b2
jr3564 <rodolfojr94@gmail.com>
2021-07-28 11:27:32 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) < 2:
+        return bool(word)
+
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word[1:-1], 0, -1)
+
+    return False
