4c26bc2f59c3c3677eb4f05c510175a4c2ddc6fe
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-07-28 19:58:59 -0300

    palíndromo recursivo =x

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
 
     sum = 0
-    
+
     if target_time is None or type(target_time) != int:
         return None
-    
+
     for item in permanence_period:
         if type(item[0]) != int or type(item[1]) != int:
             return None
         if item[0] <= target_time and target_time <= item[1]:
             sum += 1
     return sum
-
