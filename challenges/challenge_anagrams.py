44852363b40e02fa99b842927dd91c8521713142
marcelo ivan <sagacello@hotmail.com>
2021-07-28 11:20:10 -0300

    teste

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    for item in first_string:
+        lista = [letter == item for letter in second_string]
+    if (
+        len(lista) == len(first_string)
+        and first_string != ""
+        and second_string != ""
+    ):
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
-    elif word[0] != word[-1]:
-        return False
+    elif len(word) < 2:
+        return True
     else:
-        len(word) < 2
-        return is_palindrome_recursive(word[1:-1])
+        word[low_index] == word[high_index]
+        return is_palindrome_recursive(
+            word[1:-1], low_index, len(word[1:-1]) - 1
+        )
