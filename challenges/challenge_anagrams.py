b0c41c5ca6c45d72b9c80d20981b1f49f01c59a4
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-28 16:26:25 -0300

    Req 03 - 27/07/2021

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,38 @@
 def is_anagram(first_string, second_string):
-    pass
+    word = ""
+    if len(first_string) != len(second_string):
+        return False
+    for l1 in first_string:
+        if l1 in word:
+            return False
+        elif l1 in second_string:
+            word += l1
+        else:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    first_string = "amor"
+    second_string = "roma"
+    print(is_anagram(first_string, second_string))
+    # saída: True
+
+    first_string = "pedra"
+    second_string = "perda"
+    print(is_anagram(first_string, second_string))
+    # saída: True
+
+    first_string = "pato"
+    second_string = "tapo"
+    print(is_anagram(first_string, second_string))
+    # saída: True
+
+    first_string = "coxinha"
+    second_string = "empada"
+    print(is_anagram(first_string, second_string))
+    # saída: False
+
+    first_string = "pedra"
+    second_string = "pedro"
+    print(is_anagram(first_string, second_string))
