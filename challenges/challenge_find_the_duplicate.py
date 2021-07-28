4acdf3dc7a3f46e8f2a95a61822fd796518df2d7
cezar88augusto <cezar88augusto@gmail.com>
2021-07-28 01:15:38 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or len(nums) == 1:
+        return False
+
+    sort_nums = sorted(nums)
+
+    for index in range(1, len(sort_nums)):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+        if sort_nums[index] == sort_nums[index - 1]:
+            return sort_nums[index]
+
+    return False
