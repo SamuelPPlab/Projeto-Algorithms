1c3e7d7538aa56e48f604971d9f579f2ce895800
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2021-07-27 21:59:30 -0300

    Concluindo requisito 3 com a ajude de Luciano Berchon

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,42 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    for first in first_string:
-        for second in second_string:
-            if first == second:
-                return True
+    if first_string == "" or second_string == "":
         return False
+    sorted_first_string = quicksort(
+        list(first_string), 0, len(first_string) - 1
+    )
+    sorted_second_string = quicksort(
+        list(second_string), 0, len(second_string) - 1
+    )
+    for index in range(len(first_string)):
+        if sorted_first_string[index] != sorted_second_string[index]:
+            return False
+    return True
 
 
-print(is_anagram('pedrra', 'pedraa'))
+def quicksort(array, low, high):
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition_index = partition(array, low, high)
+
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+
+    return array
+
+
+def partition(array, low, high):
+    i = low - 1
+    pivot = array[high]
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
