a5b75799a50a769c91f03ad863871eaa7c0437ab
Lfocosi <luccafocosi@gmail.com>
2021-07-28 14:34:00 -0300

    ops

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 
 def is_anagram(first_string, second_string):
-return Counter(first_string) == Counter(second_string)
+    return Counter(first_string) == Counter(second_string)
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,6 +2,6 @@ def find_duplicate(nums):
     for numero in nums:
         if type(numero) == str or numero < 0:
             return False
-        elif nums.count(num) > 1:
+        elif nums.count(numero) > 1:
             return numero
     return False
