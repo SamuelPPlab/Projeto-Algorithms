c3b82a91651c9cc165199e122d482d97ef1d84b5
nevesfreire <nando.neves@gmail.com>
2021-07-27 21:10:13 -0300

    quase 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,11 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
-    if type(nums) is list:
+    try:
         i = 0
         lista = nums.sort()
         while i < len(lista) - 1:
             if lista[i] == lista[i+1]:
                 return lista[i]
-    return False
+            i += 1
+    except TypeError:
+        return False
