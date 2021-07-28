c97bf1b13f531a92329d08e6d5e3016457b06f7a
Filipi Firmino <contatoic3web@gmail.com>
2021-07-28 19:30:53 -0300

    Challenge duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,9 +1,22 @@
+def isValid(array):
+    if isinstance(array[0], str):
+        return False
+    else:
+        return True
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
-    if len(nums) > 2:
+    if len(nums) >= 2 and isValid(nums):
+        counter = 0
         result = max(set(nums), key=nums.count)
-        if result != 1:
+        for index in nums:
+            if index == result and index > 0:
+                counter += 1
+        if counter > 1:
             return result
         else:
             return False
-    return False
+    else:
+        return False
+
+# find_duplicate(['1', -2, 3])
