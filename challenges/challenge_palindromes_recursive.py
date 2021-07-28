592caf5f3468825fb2a1280a18b88069f4f66e38
danwhat <daanwhat@gmail.com>
2021-07-28 16:29:24 -0300

    quase

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,12 @@
-def is_palindrome_recursive(word, low_index, high_index):
+def is_palindrome_recursive(word, low_index=0, high_index=0):
+    print(word)
     if not word:
         return False
-    return word == word[::-1]
+    if word[0] != word[-1]:
+        return False
+    if len(word) <= 3:
+        print("returning true")
+        return True
+    is_palindrome_recursive(word[1:-1])
+
+is_palindrome_recursive("SOCOS")
