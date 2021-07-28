cbb33a5faa4e25ee5f36f109efeef1bf48a4737e
nevesfreire <nando.neves@gmail.com>
2021-07-27 23:04:11 -0300

    teste

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -3,8 +3,13 @@ def is_palindrome_iterative(word):
     i = 0
     if word == '':
         return False
-    while i < len(word) // 2 + 1:
-        if word[i] != word[len(word)-i-1]:
+    
+
+    while i < len(word) // 2:
+        if word[i] != word[len(word) - i - 1]:
             return False
         i += 1
     return True
+
+
+print(is_palindrome_iterative('amatama'))
