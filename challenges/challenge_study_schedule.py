320273a23e72aeb0d473250bda226f20559d0b85
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-07-28 18:28:45 -0300

    first commit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+
+    if not target_time:
+        return None
+    
+    for i in range(0, len(permanence_period)):
+        if type(permanence_period[i][0]) != int or type(permanence_period[i][1]) != int:
+            return None
+        if permanence_period[i][0] <= target_time <= permanence_period[i][1]:
+            counter += 1
+    
+    return counter
