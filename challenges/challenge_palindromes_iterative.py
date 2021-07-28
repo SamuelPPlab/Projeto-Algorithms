3fa35d0989a590a94b11296a831034a86ef2d639
cezar88augusto <cezar88augusto@gmail.com>
2021-07-28 01:34:14 -0300

    Requisito 4

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,10 +1,8 @@
-import math
-
 
 def is_palindrome_iterative(word):
     if not word:
         return False
-    array_length = math.ceil(len(word)//2)
+    array_length = len(word)//2
     count = len(word)
     count2 = 0
     array = list(word)
