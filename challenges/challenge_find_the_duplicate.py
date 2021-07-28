4625055ad5ee66972c889df2f260957936992f17
Filipi Firmino <contatoic3web@gmail.com>
2021-07-27 21:47:17 -0300

    challenge duplicate in progress

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,9 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    if len(nums) > 2:
+        result = max(set(nums), key=nums.count)
+        if result != 1:
+            return result
+        else:
+            return False
+    return False
