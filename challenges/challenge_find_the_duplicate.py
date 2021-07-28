dfac8a901d584c9b97dbc9e55ffa917463e5f27a
Berchon <berchon@gmail.com>
2021-07-27 23:56:05 -0300

    Req 4 done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,35 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    for index in range(1, len(nums)):
+        result_search = binary_search_boolean(
+            nums[index:], 0, len(nums) - 1 - index, nums[index - 1]
+        )
+        if result_search and nums[index - 1] >= 0:
+            return nums[index - 1]
+
+    return False
+
+
+def binary_search_boolean(array, low_index, high_index, value):
+    if high_index < low_index:
+        return False
+
+    middle_index = (high_index + low_index) // 2
+
+    if array[middle_index] == value:
+        return True
+    elif array[middle_index] > value:
+        return binary_search_boolean(array, low_index, middle_index - 1, value)
+    else:
+        return binary_search_boolean(
+            array, middle_index + 1, high_index, value
+        )
+
+
+array = [2, 3, 4, 10, 40]
+target = 10
+
+result = binary_search_boolean(array, 0, len(array) - 1, target)
+print(f"Elemento encontrado na posição: {result}")
