6fb15f7138d04bcbd61c7fa1d275d9297cab7039
danwhat <daanwhat@gmail.com>
2021-07-28 16:53:40 -0300

    agora foi

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,10 +1,14 @@
 def is_palindrome_recursive(word, low_index=0, high_index=0):
     print(word)
     if not word:
+        print("returning false")
         return False
     if word[0] != word[-1]:
+        print("returning false")
         return False
     if len(word) <= 3:
         print("returning true")
         return True
-    is_palindrome_recursive(word[1:-1])
+    return is_palindrome_recursive(word[1:-1])
+
+print(is_palindrome_recursive("batatab"))
