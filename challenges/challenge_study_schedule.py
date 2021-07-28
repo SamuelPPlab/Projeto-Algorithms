0a11ebd0865728fbedacad77ec1c86471177c15b
cezar88augusto <cezar88augusto@gmail.com>
2021-07-27 23:29:33 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not permanence_period or not target_time and target_time != 0:
+        return None
+
+    count = 0
+
+    for x in permanence_period:
+        if(x[0] is None or x[1] is None):
+            return None
+        if(target_time >= x[0] and target_time <= x[1]):
+            count += 1
+    return count
