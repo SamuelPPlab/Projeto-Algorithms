016e17cc7c80973f1425f29b75d4d432e2278995
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-28 18:52:35 -0300

    Req 05 - 28/07/2021

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,29 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    reverse_word = word[::-1]
+    if word == reverse_word:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    word = ""
+    print(is_palindrome_iterative(word))
+    # saída: True
+
+    word = "SOCOS"
+    print(is_palindrome_iterative(word))
+    # saída: True
+
+    word = "REVIVER"
+    print(is_palindrome_iterative(word))
+    # saída: True
+
+    word = "COXINHA"
+    print(is_palindrome_iterative(word))
+    # saída: False
+
+    word = "AGUA"
+    print(is_palindrome_iterative(word))
+    # saída: False
