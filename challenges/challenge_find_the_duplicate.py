26bf3bc37aa76e5092ac5e5697176004f26cd30e
Éric Vinícius <ericcobain3@gmail.com>
2021-07-28 16:04:09 -0300

    requisito 1

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,16 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    repeated = ''
+    sorted(nums)
+    if (len(nums) <= 1):
+        return False
+
+    for number in nums:
+        if nums.count(number) > 1:
+            repeated = number
+            instance = isinstance(number, str)
+
+    if (repeated is not None or instance is True):
+        return repeated
+    else:
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    length_arr = len(permanence_period)
+    if (target_time is None):
+        return None
+
+    students_online = 0
+    for index in range(length_arr):
+        if (None in permanence_period[index]):
+            return None
+        if((target_time >= permanence_period[index][0]) and
+           (target_time <= permanence_period[index][1])):
+            students_online += 1
+    return students_online
