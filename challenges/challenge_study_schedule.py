bfdc4e00a7b0155226b4f4cc4014d0a88720a306
hvenses <hvenses@gmail.com>
2021-07-28 19:03:29 -0300

    Commit inicial com requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    for time in permanence_period:
+        try:
+            if target_time >= time[0] and target_time <= time[1]:
+                counter += 1
+        except Exception:
+            return None
+    return counter
