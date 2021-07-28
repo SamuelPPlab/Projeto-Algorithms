92a66e0223203dec52e2da0b41647d65cca357db
jr3564 <rodolfojr94@gmail.com>
2021-07-28 16:32:26 -0300

    Requisito 2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_list = [letter for letter in first_string]
+    second_list = [letter for letter in second_string]
+
+    for index, letter in enumerate(first_list):
+        letter_index = None
+
+        for current_index in range(index, len(second_list)):
+            if letter == second_list[current_index]:
+                letter_index = second_list[current_index]
+                break
+
+        if not letter_index:
+            break
+
+        second_list[index], second_list[current_index] = (
+            second_list[current_index],
+            second_list[index],
+        )
+
+    return first_list == second_list
