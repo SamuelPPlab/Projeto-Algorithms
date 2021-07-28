9c7ed3b28e2cc8571ee9c688f2b570723e049135
Éric Vinícius <ericcobain3@gmail.com>
2021-07-28 11:15:39 -0300

    requisitos 2 e 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if (len(word) == 0):
+        return False
+
+    if (word == word[::-1]):
+        return True
+
+    return False
