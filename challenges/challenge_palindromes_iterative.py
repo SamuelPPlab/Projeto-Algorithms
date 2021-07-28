c2c7397f1de7b859006266dece32f5a2cb456b6c
marcelo ivan <sagacello@hotmail.com>
2021-07-28 10:10:29 -0300

    requisitos 1 2 3 e 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,8 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    valida = [
-        item
-        for item in range(len(word) // 2)
-        if not word[item] != word[len(word) - 1 - item]
-    ]
     for item in range(len(word) // 2):
         if word[item] != word[len(word) - 1 - item]:
             return False
-    return valida
+    return True
+
