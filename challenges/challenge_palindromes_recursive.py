2975df7b50bc9e5d62dd3079ad2311445c0165bf
marcelo ivan <sagacello@hotmail.com>
2021-07-28 10:01:01 -0300

    requisitos 1 2 e 3

---
+++ challenges/challenge_palindromes_recursive.py
@@ -0,0 +1,11 @@
+def is_palindrome_recursive(word, low_index, high_index):
+    if not word:
+        return False
+    return word == reverse(word)
+
+
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reverse(list[1:]) + list[0]
