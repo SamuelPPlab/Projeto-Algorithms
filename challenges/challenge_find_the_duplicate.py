26000fde41dcd01507774ce1386e2c44b3e666ca
nevesfreire <nando.neves@gmail.com>
2021-07-27 22:24:12 -0300

    final

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,11 +1,20 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
     try:
-        i = 0
-        lista = nums.sort()
-        while i < len(lista) - 1:
-            if lista[i] == lista[i+1]:
-                return lista[i]
-            i += 1
+        if nums[0] < 0:
+            return False
+        return new_function(nums)
     except TypeError:
         return False
+    except IndexError:
+        return False
+
+
+def new_function(nums):
+    lista = sorted(nums)
+    i = 0
+    while i < len(lista) - 1:
+        if lista[i] == lista[i+1]:
+            return lista[i]
+        i += 1
+    return False
