bcd74e48c9a79b7f377e0f48e7abb3348f95981f
Filipi Firmino <contatoic3web@gmail.com>
2021-07-29 01:04:37 -0300

    challege recirsive palindromes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def sort_string(list_string):
     list_result = []
-    for char in list_string:
+    for char in list(list_string):
         for index in list_string:
             if char < index:
                 list_result.append(char)
@@ -9,7 +9,8 @@ def sort_string(list_string):
 
 def is_anagram(first_string, second_string):
     if first_string != "" and second_string != "":
-        list_str1 = sort_string(list(first_string))
-        list_str2 = sort_string(list(second_string))
-        return(list_str1 == list_str2)
+        if sort_string(first_string) == sort_string(second_string):
+            return True
+        else:
+            return False
     return False
