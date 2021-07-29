3be6d9890a5381f2379baaa6d9abf06e09a267db
lisvalver <lisvalver.atte@gmail.com>
2021-07-28 22:14:57 -0300

    Req 01 - Study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui.  """
+    count = 0
+
+    if target_time is None or type(target_time) != int:
+        return None
+
+    for login, logout in permanence_period:
+        if type(login) != int or type(logout) != int:
+            return None
+        if login <= target_time <= logout:
+            count += 1
+
+    return count
