1b41d00f9dca5a5772c4044d377da7a8211d9e0e
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-29 19:07:12 -0300

    fix: additional check for is_anagram method

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,10 @@
 def is_anagram(first_string, second_string):
     # return Counter(first_string) == Counter(second_string)
-    if (len(first_string) != len(second_string)):
+    if (
+        not isinstance(second_string, str)
+        or not isinstance(first_string, str)
+        or len(first_string) != len(second_string)
+    ):
         return False
     first_dict = (
         {i: first_string.count(i) for i in first_string}
