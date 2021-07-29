b5f5141d44bd632351778f48e964d3c98ebdfada
Filipi Firmino <contatoic3web@gmail.com>
2021-07-29 01:07:00 -0300

    Fix lint error

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,3 @@
-from os import truncate
-
-
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if word != "":
@@ -9,8 +6,8 @@ def is_palindrome_recursive(word, low_index, high_index):
         elif word[low_index] != word[high_index]:
             return False
         elif low_index < high_index + 1:
-            return is_palindrome_recursive(word, low_index +1, high_index -1)
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         else:
             return True
-    else: 
+    else:
         return False
