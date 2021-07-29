90dfc9cc2a6b96c997f737a36f2e102f6cc2b102
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-29 18:41:16 -0300

    style/fix: change Counter() for a custom algorythm

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,11 @@
-from collections import Counter
-
-
 def is_anagram(first_string, second_string):
-    return Counter(first_string) == Counter(second_string)
+    # return Counter(first_string) == Counter(second_string)
+    if (len(first_string) != len(second_string)):
+        return False
+    first_dict = (
+        {i: first_string.count(i) for i in first_string}
+    )
+    second_dict = (
+        {a: second_string.count(a) for a in second_string}
+    )
+    return first_dict == second_dict
