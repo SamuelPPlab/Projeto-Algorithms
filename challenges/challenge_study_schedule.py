6bd9a88e41621d6ae6c289bdea45daf5afda6df6
Filipi Firmino <contatoic3web@gmail.com>
2021-07-28 23:24:49 -0300

    challenge study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
+def isValid(entrie):
+    for index in entrie:
+        if None in index:
+            return False
+        else:
+            return True
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time != None and isValid(permanence_period):
+        for item in permanence_period:
+           if target_time >= item[0] and target_time <= item[1]:
+               counter +=1
+        return counter
+    else:
+        return None
