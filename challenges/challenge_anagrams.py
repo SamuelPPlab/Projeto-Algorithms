4e909f616d15a2a2adc7e4babc0f52db003a3bcb
Filipi Firmino <contatoic3web@gmail.com>
2021-07-29 00:24:19 -0300

    fix error lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,9 +1,9 @@
 def sort_string(list_string):
     list_result = []
     for char in list_string:
-       for index in list_string:
-           if char < index:
-               list_result.append(char)
+        for index in list_string:
+            if char < index:
+                list_result.append(char)
     return list_result
 
 
@@ -13,5 +13,3 @@ def is_anagram(first_string, second_string):
         list_str2 = sort_string(list(second_string))
         return(list_str1 == list_str2)
     return False
-
-
