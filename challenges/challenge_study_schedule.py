9c14fa53605b5dd6d7777b05d5c2da082afc2b2a
Filipi Firmino <contatoic3web@gmail.com>
2021-07-28 23:32:52 -0300

    fix error lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,12 +5,13 @@ def isValid(entrie):
         else:
             return True
 
+
 def study_schedule(permanence_period, target_time):
     counter = 0
-    if target_time != None and isValid(permanence_period):
+    if target_time is not None and isValid(permanence_period):
         for item in permanence_period:
-           if target_time >= item[0] and target_time <= item[1]:
-               counter +=1
+            if target_time >= item[0] and target_time <= item[1]:
+                counter += 1
         return counter
     else:
         return None
