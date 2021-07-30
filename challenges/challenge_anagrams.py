988d72920f94ad46a5d9f75982620d415a06cd0b
Raphael Caputo <faelelf2@gmail.com>
2021-07-29 21:24:09 -0300

    Enviando requisitos mas com algumas pendencias

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui"""
+    # check if the first string and the second string is anagram
+    if len(first_string) != len(second_string):
+        return False
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    first_string_list = list(first_string)
+    second_string_list = list(second_string)
+
+    if first_string_list == second_string_list:
+        return True
+    else:
+        return False
