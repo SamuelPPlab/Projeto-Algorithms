23eb3a14d7c1a21236816eb5a9197890c0863a6b
Raphael Caputo <faelelf2@gmail.com>
2021-07-29 22:34:55 -0300

    Tentando o req achar duplicados, mas faltando 2 requisitos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums or nums != int:
+        return False
+    # encontre o número de elementos
+    n = len(nums)
+    # armazenar os elementos
+    s = set()
+    # iterar através da matriz
+    for i in range(n):
+        # verifique se o elemento está presente
+        if nums[i] in s:
+            return nums[i]
+        # adicione o elemento
+        s.add(nums[i])
+    return -1
