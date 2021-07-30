594151716c94cbffe92056f21e77f9a8eda19eb5
Kesley Lahoz <namielahoz@gmail.com>
2021-07-30 19:16:19 -0300

    3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    list1 = list(first_string)
+    list2 = list(second_string)
+
+    if len(list1) != len(list2):
+        return False
+
+    for letter in list1:
+        try:
+            index = list2.index(letter)
+        except ValueError:
+            return False
+
+        list2.remove(list2[index])
+
+    return True
