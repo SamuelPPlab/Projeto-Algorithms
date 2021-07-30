0f7803fe5e5c2817f6eb95c04e034863b73caff0
Kesley Lahoz <namielahoz@gmail.com>
2021-07-30 18:48:23 -0300

    1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if type(target_time) != int:
+        return None
+    active_students = 0
+    for initial, final in permanence_period:
+        if type(initial) != int or type(final) != int:
+            return None
+        if initial <= target_time <= final:
+            active_students += 1
+    return active_students
