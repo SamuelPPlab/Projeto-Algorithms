a344eabb6d47c6fe367a0d3154e26acd593217b6
Raphael Caputo <faelelf2@gmail.com>
2021-07-29 21:03:59 -0300

    Palavra iterativa eh palindromo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    # verifique se a palavra não é iterativa
+    if len(word) <= 1:
+        return False
+    # itere através da palavra e verifique se a palavra é igual ao seu reverso
+    for i in range(len(word)):
+        if word[i] != word[-i - 1]:
+            return False
+    return True
