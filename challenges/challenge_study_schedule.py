7a9e89a5398f9de28b28e80ab848da5ad00c3acf
Raphael Caputo <faelelf2@gmail.com>
2021-07-29 22:05:31 -0300

    Enviando study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        count = 0
+        for i in permanence_period:
+            if i[0] <= target_time <= i[1]:
+                count += 1
+        return count
+    except TypeError:
+        return None
