8d63fc04f450d310ea54601f5eefc961697a60c0
Camila Valentim <camilaifpb@hotmail.com>
2021-07-31 20:15:23 -0300

    projeto Algorithms

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if sorted(first_string) == sorted(second_string):
+        print("true")
+    elif (first_string == "") or (second_string == ""):
+        print("false")
+    else:
+        print("false")
+
+
+first_string = "pedra"
+second_string = ""
+is_anagram(first_string, second_string)
