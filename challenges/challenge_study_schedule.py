2cf95681f8fd6304dbf07827bef8ece54897d58b
alvarovascon <alvarovascon2@gmail.com>
2021-07-31 13:38:45 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    present_students = 0
+    for period in permanence_period:
+        if period[0] is None or period[1] is None or target_time is None:
+            return None
+        if period[0] <= target_time <= period[1]:
+            present_students += 1
+    return present_students
