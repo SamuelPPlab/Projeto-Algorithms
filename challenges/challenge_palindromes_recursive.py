cc0d6ad24b27eeb91763ce236406bbff71d61f78
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-07-31 10:57:59 -0300

    Feature: adiciona requisito 1

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if(len(word) == 0):
+        return False
+    if(word[low_index] != word[high_index]):
+        return True
+    if(low_index < high_index + 1):
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
