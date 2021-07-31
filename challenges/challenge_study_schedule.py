0c8696066194f55754819e413b634f8abf7d9d63
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-07-31 17:12:38 -0300

    requisito 1 v1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+def is_not_type_int(entry):
+    return (not type(entry[0]) == int) or (not type(entry[1]) == int)
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (not target_time):
+        return None
+
+    count = 0
+
+    for item in permanence_period:
+        if is_not_type_int(item):
+            return None
+        if item[0] <= target_time <= item[1]:
+            count += 1
+
+    return count
