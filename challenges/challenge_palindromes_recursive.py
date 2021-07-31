6859251d3c63a48e43c80d193259ce5c62e6b26d
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-07-30 23:09:16 -0300

    Req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if not word:
+        return False
+    elif len(word) <= 1:
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word[1:-1], 0, len(word[1:-1]) - 1)
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    word = "SSNSS"
+    print(is_palindrome_recursive(word, 0, len(word) - 1))
