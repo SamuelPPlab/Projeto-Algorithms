5ced13402590b6a9adbe6a701b57b7e04b4748f7
Camila Valentim <camilaifpb@hotmail.com>
2021-07-31 20:44:02 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word is None:
+    if len(word) == 0:
         return False
-    if (word[low_index] != word[high_index]):
+    if word[low_index] != word[high_index]:
         return False
-    if (low_index == high_index):
+    if low_index == high_index:
         return True
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
