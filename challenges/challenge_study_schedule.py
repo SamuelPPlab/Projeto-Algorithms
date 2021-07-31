df5db52810e4f1571bf27c398e7da423ae00e3f8
Carlos Souza <carlos.pessoal@hotmail.com>
2021-07-31 16:27:24 -0300

    Primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    avaliable_students = 0
+    try:
+        for initial_time, final_time in permanence_period:
+            if (initial_time <= target_time <= final_time):
+                avaliable_students += 1
+        return avaliable_students
+    except TypeError:
+        return None
