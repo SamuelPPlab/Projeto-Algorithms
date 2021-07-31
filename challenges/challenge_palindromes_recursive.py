4b4522e50cff5571d4ac6ecee9d1b4a3409a8cae
PedroMarquesFr2 <pedromarques1011@gmail.com>
2021-07-31 12:06:02 -0300

    feito reqs 1 e 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word is None or word == "":
+        return False
+    if low_index > high_index:
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
+
+
+# word = "AGUA"
+# print(is_palindrome_recursive(word, 0, len(word) - 1))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None or target_time == "0":
+        return None
+
+    for student in permanence_period:
+        if (
+            student[0] is None
+            or student[0] == "0"
+            or student[1] is None
+            or student[1] == "0"
+        ):
+            return None
+        if student[0] <= target_time and student[1] >= target_time:
+            counter += 1
+    return counter
+
+
+permanence_periods = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+print(study_schedule(permanence_periods, 5))
