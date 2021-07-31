e5e3f90d49bb067895930151c44cb837f02b7391
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-07-30 21:02:55 -0300

    First Commit and Req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    for item in permanence_period:
+        try:
+            if target_time >= item[0] and target_time <= item[1]:
+                counter += 1
+        except TypeError:
+            return None
+    return counter
+
+
+# if __name__ == "__main__":
+#     permanence_periods = [(0, "0"), (4, 4), (1, 3), (3, 4), (2, 5)]
+#     print(study_schedule(permanence_periods, 5))
