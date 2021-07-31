fbd4e9d4997d1e09f5423d530123e79b97e93521
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-07-31 17:29:37 -0300

    add palindrome link

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -6,3 +6,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[0] != word[-1]:
         return False
     return is_palindrome_recursive(word[1:-1], 0, 0)
+
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
