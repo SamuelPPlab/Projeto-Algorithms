32970eb7da016b06fd4930ae125483def31c262e
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-07-31 17:27:08 -0300

    Requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    list1 = list_sort(list(first_string))
+    list2 = list_sort(list(second_string))
+
+    if list1 == list2:
+        return True
+    return False
+
+
+def list_sort(inlist):
+    # ordenação lista @luc-zago
+    if inlist == []:
+        return []
+    else:
+        pivot = inlist[0]
+        lesser = list_sort([x for x in inlist[1:] if x < pivot])
+        greater = list_sort([x for x in inlist[1:] if x >= pivot])
+        return lesser + [pivot] + greater
