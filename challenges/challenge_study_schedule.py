42ccc330ed1d9e5fb2cfbff6b870fff30c22cb47
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-07-31 16:07:34 -0300

    Requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        students_online = 0
+        for student_period in permanence_period:
+            if (
+                student_period[0] <= target_time
+                and student_period[1] >= target_time
+            ):
+                students_online += 1
+        return students_online
+    except TypeError:
+        return None
