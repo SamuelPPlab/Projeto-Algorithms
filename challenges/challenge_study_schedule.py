4263b6d7dce646b8c852e8adbb90e668d7f13a14
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-07-31 11:44:25 -0300

    Feature: adiciona req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    teste = 0
+    for hour in permanence_period:
+        try:
+            if hour[0] <= target_time and hour[1] >= target_time:
+                teste = teste + 1
+        except TypeError:
+            return None
+            break
+    return teste
