5d7dc80172f28e0ffded03c9a9342a4ef9317894
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-07-31 20:18:49 -0300

    requisito 3 v1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aquiiiii. """
+
+    if len(first_string) != len(second_string):
+        return False
+
+    for i in range(len(first_string)):
+        if not first_string[i] in second_string:
+            return False
+
+    return True
