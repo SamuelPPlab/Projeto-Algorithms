067ebc73bfb68d6ee22195d8a0868bf1c38f9f21
Carlos Souza <carlos.pessoal@hotmail.com>
2021-07-31 16:28:58 -0300

    Requisitos bônus

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
+def return_duplicated_value(nums):
+    nums.sort()
+    for i in range(1, len(nums)):
+        if (nums[i] == nums[i-1]):
+            return nums[i]
+
+    return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if (len(nums) == 0):
+        return False
+
+    for num in nums:
+        if isinstance(num, int) is False or num < 1:
+            return False
+
+    return return_duplicated_value(nums)
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    reversed_word = word[::-1]
+    return reversed_word == word
