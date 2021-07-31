0bd193818834d401f6729dd24712b9377c0d3f71
wberilo <wberilo@gmail.com>
2021-07-31 11:26:21 -0300

    requisitos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(qsort(list(first_string)) == qsort(list(second_string))):
+        return True
+    else:
+        return False
+
+
+def qsort(inlist):
+    if inlist == []:
+        return []
+    else:
+        pivot = inlist[0]
+        lesser = qsort([x for x in inlist[1:] if x < pivot])
+        greater = qsort([x for x in inlist[1:] if x >= pivot])
+        return lesser + [pivot] + greater
