5704379822bec6f4f3204b82ce23fe525c2c6361
Carol Bezerra <carol.arbe@gmail.com>
2021-08-01 13:46:51 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    all_times = []
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if None in period or target_time is None:
+            return None
+        diff_time = period[1] - period[0]
+        if diff_time == 0:
+            all_times.append(period[1])
+        elif diff_time == 1:
+            all_times.append(period[0])
+            all_times.append(period[1])
+        else:
+            interm = 0
+            while interm < period[1]:
+                interm += 1
+                all_times.append(interm)
+
+    return all_times.count(target_time)
