98156e4023406206fdf6d6832f55ed2a6b8441ca
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-08-01 12:53:17 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    if word[low_index] == word[high_index]:
+        if low_index + 1 >= high_index - 1:
+            return True
+
+        if is_palindrome_recursive(word, low_index + 1, high_index - 1):
+            return True
+        else:
+            return False
+    else:
+        return False
