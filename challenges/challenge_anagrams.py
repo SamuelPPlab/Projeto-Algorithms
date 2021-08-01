664fcbe8639de2c427ca7f9e3f6a1842d85e1b5d
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-01 10:38:14 -0300

    foi o anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def sort_this_string(string):
+    array_string = list(string)
+    for i in range(len(array_string)):
+        current_value = array_string[i]
+        current_position = i
+        while (
+            current_position > 0
+            and array_string[current_position - 1] > current_value
+        ):
+            array_string[current_position] = array_string[current_position - 1]
+            current_position = current_position - 1
+        array_string[current_position] = current_value
+    return array_string
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if(len(first_string) < 1 or len(second_string) < 1): return False
+    return sort_this_string(first_string) == sort_this_string(second_string)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,4 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # if len(word) < 2: return True
+    # if word[low_index] != word[high_index]: return False
+    # return is_palindrome_recursive(word[low_index+1:high_index-1],)
