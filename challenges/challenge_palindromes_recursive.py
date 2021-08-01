f2b7d665de5527e66399054e3651c2f23853ba9d
Lucas Andrade <lafdesouza2002@outlook.com>
2021-07-31 22:12:56 -0300

    Tentando diminuir complexidade

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,8 +3,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
     elif low_index == high_index:
-        if list(word)[low_index] == list(word)[high_index]:
-            return True
+        return True
     elif list(word)[low_index] == list(word)[high_index]:
         if low_index + 1 == high_index:
             return True
