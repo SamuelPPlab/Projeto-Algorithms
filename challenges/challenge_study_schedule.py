a434fb9b102c0923505f65235f8d793d79ae6e9b
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-08-01 13:28:26 -0300

    Envio do reuquisito 1 para avaliaçao

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aquiiii. """
+    try:
+        initial_counter = 0
+        for time in permanence_period:
+            if time[0] <= target_time and time[1] >= target_time:
+                initial_counter += 1
+        return initial_counter
+    except TypeError:
+        return None
