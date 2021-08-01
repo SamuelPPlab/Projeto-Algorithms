232d1b39bc4b76cd610c42134937c999b6a20ff8
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-01 01:21:16 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    arr_second_string = list(second_string)
+    copy_arr_second_string = arr_second_string.copy()
+    for i in range(len(first_string)):
+        if first_string[i] not in copy_arr_second_string:
+            return False
+        else:
+            arr_second_string[i] = first_string[i]
+            copy_arr_second_string.remove(first_string[i])
+    if first_string == ''.join(arr_second_string):
+        return True
+    else:
+        return False
