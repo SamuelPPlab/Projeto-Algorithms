c076f444cf448f680942717859804478aaa1e874
Strongreen <strongreen@live.com>
2021-08-01 15:01:05 -0300

    First Commit

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 1: return True
+    if not word: return False
+    reversed_list = []
+    for item in list(word):
+        reversed_list.insert(0, item)
+    if word == ''.join(reversed_list): return True
+    else: return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    word_list = list(word)
+    if ((low_index - high_index)< 2):
+        return True
+    if (word_list[high_index] != word_list[low_index]):
+        return False
+    return is_palindrome_recursive(word_list, low_index, high_index ]
