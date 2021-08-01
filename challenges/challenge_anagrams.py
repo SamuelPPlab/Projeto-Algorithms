4d027db9293e186bb46c98f15f0ccb0ecdc286dc
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-08-01 16:29:52 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+
+    letters_first = first_string
+    letters_second = second_string
+
+    for letter in first_string:
+        letters_first = letters_first.replace(letter, "")
+        letters_second = letters_second.replace(letter, "")
+
+        if len(letters_first) != len(letters_second):
+            return False
+
+    return True
