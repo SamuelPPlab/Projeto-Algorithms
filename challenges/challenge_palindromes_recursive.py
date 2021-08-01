e4dcd8767af3ff5bc9b058c19d5691e9b85528e8
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-08-01 19:53:32 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "" or word[low_index] != word[high_index]:
+        return False
+    word = word[1:high_index]
+    if len(word) == 1 or word == "":
+        return True
+    return is_palindrome_recursive(word, 0, len(word) - 1)
+
