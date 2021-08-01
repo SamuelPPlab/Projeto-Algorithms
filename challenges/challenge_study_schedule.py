d9e6d8614761e96feb702c4a19ffb30589d8eb72
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-08-01 12:23:26 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    target_time_rate = 0
+    
+    if target_time is None: return None
+
+    for period in permanence_period:
+        if (period[0] is None or period[1] is None):
+            return None
+
+        if int(period[0]) <= int(target_time) <= int(period[1]):
+            target_time_rate += 1
+        
+    return target_time_rate
