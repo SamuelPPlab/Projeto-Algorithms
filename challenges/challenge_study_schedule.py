35008b9a1a66be6ecd8383a05ea05694bc6e399a
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-08-01 12:25:15 -0300

    lint requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,8 @@
 def study_schedule(permanence_period, target_time):
     target_time_rate = 0
-    
-    if target_time is None: return None
+
+    if target_time is None:
+        return None
 
     for period in permanence_period:
         if (period[0] is None or period[1] is None):
@@ -9,5 +10,5 @@ def study_schedule(permanence_period, target_time):
 
         if int(period[0]) <= int(target_time) <= int(period[1]):
             target_time_rate += 1
-        
+
     return target_time_rate
