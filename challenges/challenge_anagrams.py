1eeaa5dfdb108ddeb1c38bd4f45652901a150f30
Carol Bezerra <carol.arbe@gmail.com>
2021-08-01 15:24:50 -0300

    avanço no Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,9 @@
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
+
+    for letter in first_string:
+        if second_string.find(letter) != -1:
+            return True
+        else:
+            return False
