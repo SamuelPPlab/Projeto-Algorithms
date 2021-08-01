68de6f9cdc768d8d475e37ab8f530436c4a66afe
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-01 02:00:54 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not target_time:
+        return None
+    c = 0
+    for i in permanence_period:
+        if not i[0] or not i[1]:
+            return None
+        if i[0] <= target_time <= i[1]:
+            c += 1
+    return c
