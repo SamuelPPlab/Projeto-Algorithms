3abbcd2cf0360c5a98ef4ba697e9ecac1518817a
Camila Valentim <camilaifpb@hotmail.com>
2021-07-31 21:44:39 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,12 +1,18 @@
+def listSorted(string):
+    my_list = []
+    for i in range(len(string)):
+        for key, value in enumerate(my_list):
+            if string[i] < value:
+                my_list.insert(key, string[i])
+                break
+        else:
+            my_list.append(string[i])
+    return my_list
+
+
 def is_anagram(first_string, second_string):
-    if sorted(first_string) == sorted(second_string):
-        print("true")
-    elif (first_string == "") or (second_string == ""):
-        print("false")
-    else:
-        print("false")
+    if listSorted(first_string) == listSorted(second_string):
+        return True
+    else: 
+        return False
 
-
-first_string = "pedra"
-second_string = ""
-is_anagram(first_string, second_string)
