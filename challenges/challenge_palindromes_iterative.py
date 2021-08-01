a69a427a33481264d79b27f36ba814bc66143d11
Strongreen <strongreen@live.com>
2021-08-01 15:14:30 -0300

    corrigindo flake8

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,12 @@
 def is_palindrome_iterative(word):
-    if len(word) == 1: return True
-    if not word: return False
+    if len(word) == 1:
+        return True
+    if not word:
+        return False
     reversed_list = []
     for item in list(word):
         reversed_list.insert(0, item)
-    if word == ''.join(reversed_list): return True
-    else: return False
+    if word == ''.join(reversed_list):
+        return True
+    else:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     word_list = list(word)
-    if len(word) < 1: return False
-    if low_index == high_index: return True
-    if word_list[high_index] != word_list[low_index]: return False
+    if len(word) < 1:
+        return False
+    if low_index == high_index:
+        return True
+    if word_list[high_index] != word_list[low_index]:
+        return False
     return is_palindrome_recursive(word_list, low_index + 1, high_index - 1)
