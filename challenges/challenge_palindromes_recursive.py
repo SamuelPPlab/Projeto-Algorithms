e2947fb383c10c9b2d3263ff2fc77e2097e26f6f
Strongreen <strongreen@live.com>
2021-08-01 15:10:54 -0300

    consertando palindromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
     word_list = list(word)
-    if ((low_index - high_index)< 2):
-        return True
-    if (word_list[high_index] != word_list[low_index]):
-        return False
-    return is_palindrome_recursive(word_list, low_index, high_index ]
+    if len(word) < 1: return False
+    if low_index == high_index: return True
+    if word_list[high_index] != word_list[low_index]: return False
+    return is_palindrome_recursive(word_list, low_index + 1, high_index - 1)
