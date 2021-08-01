7e0b1f4dea3a4699e572bbae3116510f46d97527
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-08-01 13:15:33 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    if (len(first_string) == len(second_string)):
+        for character in first_string:
+            index = second_string.find(character)
+            if index == -1:
+                return False
+            else:
+                second_string = second_string.replace(character, '', 1)
+    else:
+        return False
+
+    return True
