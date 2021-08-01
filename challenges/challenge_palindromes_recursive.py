09046ef95e8b9f54a366dc5a719af0c9407483f6
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-01 12:08:42 -0300

    foi o palindromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    return False
+    if (
+        len(word) < 1
+        or word is None
+    ):
+        return False
+    if (
+        len(word) <= 2
+        and word[0] == word[len(word) - 1]
+    ):
+        return True
+    if (word[0] == word[len(word) - 1]):
+        return is_palindrome_recursive(
+            word[1: len(word) - 1], low_index, high_index - 1)
+    else:
+
+        return False
