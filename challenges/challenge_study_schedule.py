fec79f8d00b6dd17d93e707a5c21f581aa6bd966
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-01 12:38:44 -0300

    parece que foi

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_count = 0
+    if target_time is None:
+        return None
+    for study_time in permanence_period:
+        if(
+            study_time[0] is None
+            or study_time[1] is None
+        ):
+            return None
+        elif (
+            study_time[0] <= target_time
+            and target_time <= study_time[1]
+        ):
+            students_count += 1
+
+    return students_count
