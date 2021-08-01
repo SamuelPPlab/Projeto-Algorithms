83d1a9280856c8a448c64ce0c64929f588d5d9b8
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-08-01 15:34:00 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    max_student_by_time = 0
+
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                max_student_by_time += 1
+    except TypeError:
+        return None
+
+    return max_student_by_time
+
