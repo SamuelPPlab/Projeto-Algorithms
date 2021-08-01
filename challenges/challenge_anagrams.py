82f06664ccf9f5b9eeba71c12eb858cc81c64055
TIAGO <tiagoesdrasvieira@gmail.com>
2021-08-01 15:43:33 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string = list(first_string)
+    second_string = list(second_string)
+
+    if len(first_string) != len(second_string):
+        return False
+
+    j = len(second_string) - 1
+    for i in range(len(first_string)):
+        if(second_string[j] != first_string[i]):
+            return False
+        j = j - 1
+
+    return True
