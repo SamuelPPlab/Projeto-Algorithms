687bf31312f2159a50950d699f6d92bea842d6c1
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-01 23:27:11 -0300

    feat: requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word[low_index:high_index], 0, -1)
+
+# https://stackoverflow.com/questions/952110/recursive-function-palindrome-in-python
