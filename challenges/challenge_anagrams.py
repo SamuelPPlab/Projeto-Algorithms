a48099f18538c681fa8a3aa0a56d8da570dff292
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-02 01:09:45 -0300

    feat: requirement 3 firstd try

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,41 @@
+def sort_string(first_string):
+    string_array = list(first_string)
+
+    # Selection Sort
+    for i in range(len(string_array)):
+        min = i
+
+        for j in range(i + 1, len(string_array)):
+            if string_array[j] < string_array[min]:
+                min = j
+
+        string_array[min], string_array[i] = string_array[i], string_array[min]
+
+    return string_array
+
+
+def compare_string(second_string, first_string_sorted):
+    string_array = list(second_string)
+
+    for i in range(len(string_array)):
+        min = i
+
+        for j in range(i + 1, len(string_array)):
+            if string_array[j] < string_array[min]:
+                min = j
+
+        string_array[min], string_array[i] = string_array[i], string_array[min]
+
+        if string_array[i] != first_string_sorted[i]:
+            return False
+
+    return True
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_string_sorted = sort_string(first_string)
+    return compare_string(second_string, first_string_sorted)
+
+# Aula 36.3 Trybe
