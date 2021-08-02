eaf1654bff6a6e1013ef0524842a10b8a9b37462
Igor-leao <igorleao.eng@gmail.com>
2021-08-02 09:43:35 -0300

    inicio requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    counter = 0
+    in_time = 0
+    out_time = 1
+
+    for schedule in permanence_period:
+        if schedule[in_time] is None or schedule[out_time] is None:
+            return None
+
+        if schedule[in_time] <= target_time <= schedule[out_time]:
+            counter += 1
+
+    return counter
