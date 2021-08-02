a26bc330c39ebe40565574a8f841efa582f9bf89
vitorAmorims <vtamorims@gmail.com>
2021-08-02 09:03:43 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    count = 0
+    if target_time is None:
+        return None
+
+    for t0, t1 in permanence_period:
+        if type(t0) != int or type(t1) != int:
+            return None
+        if t0 <= target_time and target_time <= t1:
+            count += 1
+
+    return count
