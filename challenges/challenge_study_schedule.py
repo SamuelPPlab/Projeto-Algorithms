6fa9092d4369bccb25d89b3b38be38ac0a119791
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-01 22:56:49 -0300

    feat: requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+
+    if target_time is None:
+        return None
+
+    for student_time in permanence_period:
+        if type(student_time[0]) != int or type(student_time[1]) != int:
+            return None
+        if target_time >= student_time[0] and target_time <= student_time[1]:
+            counter += 1
+
+    return counter
