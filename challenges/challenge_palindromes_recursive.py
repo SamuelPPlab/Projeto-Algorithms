7db1aa98f1fddd100af061113e7e3696a9ce6fd3
CarolSi-hub <a.carolg1@gmail.com>
2021-08-01 21:33:13 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '' or word[low_index] != word[high_index]:
+        return False
+
+    while low_index < high_index:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+    return True
+
+# src: https://medium.com/guvi/recursive-python-program-to-test-if-a-string-is-
+# a-palindrome-or-not
+# -1eb69f2222ec
