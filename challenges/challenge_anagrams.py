337c56177654793d6da1aa81c5bc4b14d4aa5629
Matheus Beaumord <matheusbeaumord@gmail.com>
2021-08-02 12:48:37 -0300

    Envio do requisito 3 para avaliaçao

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,35 @@
+def merge_sort(array, low, high):
+    i = low - 1
+    pivot = array[high]
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+
+
+def merge(array, low, high):
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition = merge_sort(array, low, high)
+
+    merge(array, low, partition - 1)
+    merge(array, partition + 1, high)
+
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_array = list(first_string)
+    second_array = list(second_string)
+
+    sorted_first_array = merge(first_array, 0, len(first_array) - 1)
+    sorted_second_array = merge(second_array, 0, len(second_array) - 1)
+
+    return "".join(sorted_first_array) == "".join(sorted_second_array)
