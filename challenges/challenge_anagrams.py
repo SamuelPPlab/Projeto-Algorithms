7e4a38e921631f4c129cde748b8ce28d8d705988
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-08-02 00:19:42 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
 def is_anagram(first_string, second_string):
-    """ --Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+
+    return "".join(insertionSort(list(first_string))) == "".join(
+        insertionSort(list(second_string))
+    )
+
+
+def insertionSort(arr):
+    for item in range(1, len(arr)):
+        key = arr[item]
+        x = item - 1
+        while x >= 0 and key < arr[x]:
+            arr[x + 1] = arr[x]
+            x -= 1
+        arr[x + 1] = key
+
+    return arr
