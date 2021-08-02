d9a4c70cf1ecf85d529c5a8b46eb3bc8f25f134d
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-02 12:41:38 -0300

    feat: requirement 3: third try

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -14,28 +14,11 @@ def sort_string(first_string):
     return string_array
 
 
-def compare_string(second_string, first_string_sorted):
-    string_array = list(second_string)
-
-    for i in range(len(string_array)):
-        min = i
-
-        for j in range(i + 1, len(string_array)):
-            if string_array[j] < string_array[min]:
-                min = j
-
-        string_array[min], string_array[i] = string_array[i], string_array[min]
-
-        if string_array[i] != first_string_sorted[i]:
-            return False
-
-    return True
-
-
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
     first_string_sorted = sort_string(first_string)
-    return compare_string(second_string, first_string_sorted)
+    second_string_sorted = sort_string(second_string)
+    return first_string_sorted == second_string_sorted
 
 # Aula 36.3 Trybe
