9054e158c4b95802184082b9ea2193b57d106475
danilocouto1 <dansantosalfa@gmail.com>
2021-08-02 18:10:19 -0300

    3 Requisitos

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 1:
+        return True
+    if not word:
+        return False
+    if word == ''.join(list(word)[::-1]):
+        return True
+    else:
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time != 0 and not target_time:
+        return None
+    count = 0
+    for inicial, final in permanence_period:
+        if (type(inicial) != int) or (type(final) != int):
+            return None
+        if (inicial <= target_time <= final):
+            count += 1
+    return count
