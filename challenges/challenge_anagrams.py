9ff7b66d2de0ec9a2908b12adc731bae4f49328f
Igor-leao <igorleao.eng@gmail.com>
2021-08-02 10:35:22 -0300

    requisito 3 incompleto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_result = selection_sort(list(first_string))
+    second_result = selection_sort(list(second_string))
+
+    return first_result == second_result
+
+
+def selection_sort(string):
+    for i in range(len(string)):
+        minimum = i
+        for j in range(i + 1, len(string)):
+            if string[j] < string[minimum]:
+                minimum = j
+        string[minimum], string[i] = string[i], string[minimum]
+
+    return string
