500007df46cc4a3aa7f68b23c7059097dc29ab08
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-08-01 21:56:33 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    students_quantity = 0
+
+    for student in permanence_period:
+        if not isinstance(student[0], int) or not isinstance(student[1], int):
+            return None
+        if target_time >= student[0] and target_time <= student[1]:
+            students_quantity += 1
+    return students_quantity
