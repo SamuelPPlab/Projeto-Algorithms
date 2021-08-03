d299141adf213d19448b3dcf8bb654749456de3a
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-03 12:38:25 -0300

    requirement n1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    # permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+    # target_time = 5
+    if not target_time:
+        return None
+
+    cont = 0
+    for element in permanence_period:
+        if type(element[0]) != int or type(element[1]) != int:
+            return None
+        if element[0] <= target_time <= element[1]:
+            cont += 1
+    return cont
