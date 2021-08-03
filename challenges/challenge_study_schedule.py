bf317061c8df0097bde7bf75dd1ca1942a626746
PedroMarqDev <PedroMarqDev@gmail.com>
2021-08-03 19:34:11 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for i in permanence_period:
+        if(i[0] is None or i[1] is None):
+            return None
+        if(target_time >= i[0] and target_time <= i[1]):
+            counter += 1
+    return counter
