be622c20f1fd12c29bf85445e7fb26b7052c9929
PedroMarqDev <PedroMarqDev@gmail.com>
2021-08-03 20:13:10 -0300

    Fix complexity

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -4,12 +4,11 @@ def is_anagram(first_string, second_string):
     if first_string == "" or second_string == "":
         return False
     counter = 0
-    first_string_array = list(first_string)
     second_string_array = list(second_string)
-    for i in range(len(first_string_array)):
+    for i in range(len(first_string)):
         for j in range(len(second_string_array)):
-            if first_string_array[i] == second_string_array[j]:
+            if first_string[i] == second_string_array[j]:
                 second_string_array.remove(second_string_array[j])
                 counter += 1
                 break
-    return counter == len(first_string_array)
+    return counter == len(first_string)
