1c5e4824a4a9ba3435a8316cd069f2568c90127b
rodrigoderkian <derkiandev@gmail.com>
2021-08-03 10:07:02 -0300

    req1/3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string or len(first_string) != len(second_string):
+        return False
+    else:
+       first_string = list(first_string.lower())
+       second_string = list(second_string.lower())
+       for letra in first_string:
+            if letra not in second_string:
+               return False
+            second_string.remove(letra)
+    return True
+
+    # https://stackoverflow.com/questions/58396590/the-most-efficient-way-testing-2-strings-for-anagrams-python
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    contador = 0
+    if not target_time:
+        return None
+    for entrada, saida in permanence_period:
+        if not entrada or not saida:
+            return None
+        if entrada <= target_time <= saida:
+            contador += 1
+    return contador
