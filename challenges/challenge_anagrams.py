77c1714096ea4021d0ccfa7a8350cb6bcd157780
CarolSi-hub <a.carolg1@gmail.com>
2021-08-02 21:11:36 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    word1 = list(first_string)
+    word2 = list(second_string)
+    if len(word1) != len(word2):
+        return False
+    else:
+        for letter in word1:
+            if letter in word2:
+                word2.remove(letter)
+        return len(word2) == 0
