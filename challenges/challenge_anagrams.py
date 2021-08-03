67a719106bbe5655e27bf151b0a73b0b837fd6aa
Macarena Rodriguez <macarenar697@gmail.com>
2021-08-03 20:56:17 -0300

    Req 5

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -28,6 +28,17 @@ if __name__ == "__main__":
 #    else:
 #        return False
 
+######################################################
+# def is_anagram(first_string, second_string):
+#    if first_string == "":
+#        return False
+#    flaw = False
+#    for i in range(len(first_string)):
+#        for e in range(len(second_string)):
+#            if first_string[i] == second_string[e]:
+#                flaw = True
+#    return flaw
+
 # def is_anagram(first_string, second_string):
 #    """ Faça o código aqui. """
 #    reverse = ''.join(list(second_string)[::-1])
