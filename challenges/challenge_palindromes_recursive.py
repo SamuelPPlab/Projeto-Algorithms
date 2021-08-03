6d578c9318710657826cd93b39746efee5e416d3
Macarena Rodriguez <macarenar697@gmail.com>
2021-08-03 19:07:34 -0300

    Req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    if low_index <= high_index:
+        if word[low_index] == word[high_index]:
+            low_index += 1
+            high_index -= 1
+            return is_palindrome_recursive(word, low_index, high_index)
+        else:
+            return False
+    return True
+
+
+# if __name__ == "__main__":
+#    word = "SOCOS"
+#    print(is_palindrome_recursive(word, 0, len(word) - 1))
