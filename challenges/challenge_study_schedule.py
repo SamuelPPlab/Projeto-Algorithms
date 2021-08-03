59514e2ed3e4e914ffaac956b154b97f7cfb1a42
Macarena Rodriguez <macarenar697@gmail.com>
2021-08-02 22:00:55 -0300

    Primeiro commit Req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+    for i, e in permanence_period:
+        if isinstance(i, int) and isinstance(e, int):
+            if i <= target_time <= e:
+                counter += 1
+        else:
+            return None
+    return counter
+
+
+# if __name__ == "__main__":
+#    permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+#    print(study_schedule(permanence_period, 3))
