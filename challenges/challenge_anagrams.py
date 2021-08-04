09c32a4a80f044999481a697867672c085fef11b
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-08-04 06:33:49 -0300

    req03 50%

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,12 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    pass
+    str01 = list(first_string.lower())
+    str02 = list(second_string.lower())
+    if len(str01) != len(str02):
+        return False
+    if ((str01 or str02 == '') or (str01 or str02 != str)):
+        return False
+
+    return False
+
 # 3 - python3 -m pytest tests/test_anagrams.py
