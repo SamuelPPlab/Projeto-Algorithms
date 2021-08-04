5d1b1aebce3f943b3eda6ab77e1b3d793b59ed99
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-08-04 01:27:02 -0300

    1st req

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,2 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
-# só para abrir PR
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for login, logout in permanence_period:
+            if login <= target_time <= logout:
+                counter = counter + 1
+        return counter
+    except TypeError:
+        return None
