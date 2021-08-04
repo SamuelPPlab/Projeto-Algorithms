94f627a6547355cf04e4181bdefa9db3360de28d
Leandro <leandro.feitoza8018@gmail.com>
2021-08-04 12:11:43 -0300

    commit inicial

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low, high):
+    if not word:
+        return False
+    if word[low] == word[high]:
+        if low == high:
+            return True
+        return is_palindrome_recursive(word, low + 1, high - 1)
+    if word[low] != word[high]:
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
-def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+def study_schedule(start_time, end_time, target_time):
+    result = 0
+    for x1, x2 in zip(start_time, end_time):
+        if target_time >= x1 and target_time <= x2:
+            result += 1
+    return result
