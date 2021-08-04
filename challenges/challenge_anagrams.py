42f0e98ca70227e04159d253e8442166f3b5a413
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-04 11:16:20 -0300

    merge sort

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,24 +1,34 @@
-def sort_string(first_string):
-    string_array = list(first_string)
+# Merge sort
 
-    # Selection Sort
-    for i in range(len(string_array)):
-        min = i
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
 
-        for j in range(i + 1, len(string_array)):
-            if string_array[j] < string_array[min]:
-                min = j
 
-        string_array[min], string_array[i] = string_array[i], string_array[min]
-
-    return string_array
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+    return merged
 
 
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
-    first_string_sorted = sort_string(first_string)
-    second_string_sorted = sort_string(second_string)
+    first_string_sorted = merge_sort(list(first_string))
+    second_string_sorted = merge_sort(list(second_string))
     return first_string_sorted == second_string_sorted
 
 # Aula 36.3 Trybe
