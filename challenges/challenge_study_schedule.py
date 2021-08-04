66cae6f94303e761eccdead50280f9cd2514c9eb
Israel Menon <zezomenon@gmail.com>
2021-08-04 08:15:01 -0300

    Primeiro commit - requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
+    for student in permanence_period:
+        try:
+            if target_time >= student[0] and target_time <= student[1]:
+                count += 1
+        except TypeError:
+            return None
+    return count
+
+
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
