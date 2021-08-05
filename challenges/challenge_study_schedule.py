daa4f9373ce2f795285717e100a1397ce0414e48
Bruno Nogueira <nugobruno@gmail.com>
2021-08-05 20:54:00 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_present = 0
+
+    try:
+
+        for period in permanence_period:
+            arrive_hour = period[0]
+            depart_hour = period[1]
+
+            if arrive_hour <= target_time <= depart_hour:
+                students_present += 1
+
+    except TypeError:
+        return None
+    return students_present
