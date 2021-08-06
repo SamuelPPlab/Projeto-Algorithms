088b0ef5ddf483c5294bc53e77d45e45676c90a2
Bruno Nogueira <nugobruno@gmail.com>
2021-08-05 21:26:52 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+def is_palindrome_recursive(word, low, high):
+    if not word or word[low] != word[high]:
+        return False
+
+    if len(word) == 3:
+        return True
+
+    new_word = word[1:-1]
+
+    return is_palindrome_recursive(new_word, low, len(new_word) - 1)
