b2fd4414a58b084b9957345196cdc44aa16da4a7
Bruno Nogueira <nugobruno@gmail.com>
2021-08-07 20:18:52 -0300

    anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+def gen_dict(chars):
+    generate_dict = {}
+
+    for char in chars:
+        if char in generate_dict:
+            generate_dict[char] += 1
+        else:
+            generate_dict[char] = 1
+
+    return generate_dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first_string_chars = list(first_string)
+    second_string_chars = list(second_string)
+
+    first_string_dict = gen_dict(first_string_chars)
+    second_string_dict = gen_dict(second_string_chars)
+
+    return first_string_dict == second_string_dict
