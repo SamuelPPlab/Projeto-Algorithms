06d87abd9e8c3d8c0e2901316e67432b640ce862
Thays Costa <thayscosta3@gmail.com>
2021-08-07 20:55:22 -0300

    2nd req. - is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """
+        Determina se uma string é ou não palíndromo usando recusão.
+        Ref: https://bit.ly/3ixCMAZ
+    """
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+    if len(word) < 2:
+        return True
+    return is_palindrome_recursive(word[1:high_index], low_index, -1)
