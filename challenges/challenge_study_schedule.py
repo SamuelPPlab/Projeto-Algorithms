0d36478d89d1fd0d7d17308f7ae71fcce3f9f2c5
Thays Costa <thayscosta3@gmail.com>
2021-08-07 20:11:43 -0300

    1st req. - study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    """  Recebe:
+            - permanence_period:
+                - Tupla com horário de login e logout de estudantes do sistema;
+            - target_time:
+                - variável com horários a serem testados
+        Retorna:
+            - O número de estudantes estudando no mesmo horário
+            - None se permanence_period for inválido"""
+
+    active_students = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                active_students = active_students + 1
+        return active_students
+    except TypeError:
+        return None
