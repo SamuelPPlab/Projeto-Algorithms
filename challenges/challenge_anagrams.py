d78be0d4b93f8ef8ff7d630b22250f8d6e6faa20
danilocouto1 <dansantosalfa@gmail.com>
2021-08-09 12:13:30 -0300

    100%

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    palavra1 = list(first_string)
+    palavra2 = list(second_string)
+    if len(palavra1) != len(palavra2):
+        return False
+    else:
+        for letra in palavra1:
+            if letra in palavra2:
+                palavra2.remove(letra)
+        return len(palavra2) == 0
