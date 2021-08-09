df3a7a0ba58b406f1decef65bb0f8002b5862979
danilocouto1 <dansantosalfa@gmail.com>
2021-08-09 11:41:23 -0300

    Duplicate win

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,15 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    test = dict()
+    results = False
+
+    for num in nums:
+        if not isinstance(num, int) or num < 0:
+            results = False
+            break
+        elif bool(test.get(num, 0)):
+            results = num
+            break
+
+        test[num] = 1
+
+    return results
