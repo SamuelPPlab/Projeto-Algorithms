4e930ccda7988923631513c49207a87435bd7eff
Brune Miguel <bstfreire@gmail.com>
2021-08-10 11:16:55 -0300

    Palindrome Recursive e Study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students = 0
+    try:
+        for time1, time2 in permanence_period:
+            if (time1 <= target_time <= time2):
+                students += 1
+        return students
+    except TypeError:
+        return None
