43743e8dc38ac334fb6f28b54dca06794a11d115
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-10 16:46:32 -0300

    checking requirement 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,21 +1,23 @@
+def check_value(value):
+    if (type(value) is not int) or (value < 0):
+        return False
+    return True
+
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
     list_doubles = []
     nums_support = nums.copy()
-    try:
-        if (len(nums) == 0):
-            return False
-
-        for index, i in enumerate(nums):
-            del(nums_support[0])
-            if (type(i) is not int) or (i < 0):
-                return False
-            if i in nums_support:
-                list_doubles.append(i)
-
-        if len(list_doubles) == 0:
-            return False
-
-        return list_doubles[0]
-    except TypeError:
+    if (len(nums) == 0):
         return False
+
+    for i in nums:
+        del(nums_support[0])
+
+        if check_value(i) and (i in nums_support):
+            list_doubles.append(i)
+
+    if len(list_doubles) == 0:
+        return False
+
+    return list_doubles[0]
