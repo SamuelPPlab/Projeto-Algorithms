59fdc52cc57a42501d20eccd4c51ae8b8f9d050b
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-10 13:37:51 -0300

    checkint requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    li = low_index
+    hi = high_index
+    try:
+        if not word or word[li] != word[hi]:
+            return False
+
+        if len(word) < 3:
+            return True
+
+        return is_palindrome_recursive(word[1:-1], li, len(word[1:-1]) - 1)
+    except TypeError:
+        return False
