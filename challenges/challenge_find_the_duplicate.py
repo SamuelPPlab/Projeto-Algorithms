cd59876be93ad09c6ed678003d3fec6648b349f9
Brune Miguel <bstfreire@gmail.com>
2021-08-10 10:40:08 -0300

    Challenge Find Duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
+def duplicate_value(nums):
+    nums.sort()
+    for index in range(1, len(nums)):
+        if (nums[index] == nums[index-1]):
+            return nums[index]
+
+    return False
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if (len(nums) == 0):
+        return False
+
+    for num in nums:
+        if isinstance(num, int) is False or num < 1:
+            return False
+
+    return duplicate_value(nums)
