a666468ef21aaf5b0b8bbde8075d1be0e7d0b5e5
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-10 10:55:05 -0300

    checkint requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,19 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    # number = 0
+    # if target_time is None:
+    #     return None
+
+    # for student in permanence_period:
+    #     if student[0] <= target_time <= student[1]:
+    #         number += 1
+    # return number
+
+    number = 0
+    try:
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                number += 1
+        return number
+    except TypeError:
+        return None
