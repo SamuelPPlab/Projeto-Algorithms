a207da0198e8d26ee43e39f46f4279344c0fc559
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-10 11:15:26 -0300

    checkint requirement 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    try:
+        if word == "":
+            return False
+        return word == word[::-1]
+    except TypeError:
+        return False
