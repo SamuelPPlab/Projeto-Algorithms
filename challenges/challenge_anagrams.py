a4501144c1101a52b11f918c8fca2e2af8203d54
Lorena Góes <goesloren@gmail.com>
2021-08-11 16:56:16 -0300

    challenge 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_list = list(first_string)
+    second_list = list(second_string)
+
+    for letter in first_list:
+        if letter in second_list:
+            second_list.remove(letter)
+    return len(second_list) == 0
+
+# Referência: Carol Andrade
