7e9a2d10b99cba0378bbad941af5ff1c2c943dfd
Lorena Góes <goesloren@gmail.com>
2021-08-11 15:12:48 -0300

    challenge 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for start_time, end_time in permanence_period:
+            if int(start_time) <= target_time <= int(end_time):
+                counter += 1
+        return counter
+
+    except TypeError:
+        return None
