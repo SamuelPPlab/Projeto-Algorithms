39825ee76119802c442c902fd52971d0cf8d8785
mateus <mateuslaalmeida@gmail.com>
2021-08-13 11:33:18 -0300

    Desafios 1-5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for i in range(len(nums)):
+        if isinstance(nums[i], int) and nums[i] >= 0:
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return nums[i]
+    return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    low_index, high_index = 0, len(word) - 1
+    while(low_index <= high_index):
+        if(word[low_index] != word[high_index]):
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
