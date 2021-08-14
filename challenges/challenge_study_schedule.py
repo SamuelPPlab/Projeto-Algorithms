72879c97e99bd631695927e427dc478ee2ed8b69
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-14 17:31:08 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,8 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    output = 0
+    for student in permanence_period:
+        start_time = student[0]
+        end_time = student[1]
+        if start_time <= target_time and end_time >= target_time:
+            output = output + 1
+    return output
