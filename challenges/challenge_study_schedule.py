fc2675aadd0baec3e4305edb7dae0a79b128141c
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-14 20:12:46 -0300

    Refatorando requisito 1 - Tentanto diminuir tempo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
     output = 0
-    if target_time is not None:
-        for student in permanence_period:
-            start_time = student[0] if type(student[0]) == int else None
-            end_time = student[1] if type(student[0]) == int else None
-            if start_time is not None and end_time is not None:
-                if start_time <= target_time and end_time >= target_time:
-                    output = output + 1 if type(output) == int else None
-            else:
-                output = None
-        return output
-    else:
+    if not target_time:
         return None
+    for student in permanence_period:
+        start_time = student[0] if type(student[0]) == int else None
+        end_time = student[1] if type(student[0]) == int else None
+        if start_time is not None and end_time is not None:
+            if start_time <= target_time and end_time >= target_time:
+                output = output + 1
+        else:
+            return None
+    return output
