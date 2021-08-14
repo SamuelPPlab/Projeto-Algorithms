f440ed2b1a8e9b31fa6028fb82eac9e425f97aad
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-14 19:57:22 -0300

    Refatorando requisito 1 - Complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,14 @@
 def study_schedule(permanence_period, target_time):
     output = 0
-    invalid_entrance = False
     if target_time is not None:
         for student in permanence_period:
             start_time = student[0] if type(student[0]) == int else None
             end_time = student[1] if type(student[0]) == int else None
             if start_time is not None and end_time is not None:
                 if start_time <= target_time and end_time >= target_time:
-                    output = output + 1
+                    output = output + 1 if type(output) == int else None
             else:
-                invalid_entrance = True
+                output = None
+        return output
     else:
-        output = None
-    if invalid_entrance:
-        output = None
-    return output
+        return None
