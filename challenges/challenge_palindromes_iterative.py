29234f3fd9a8a979bb6c64c21b014e80101e48d9
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-14 21:15:16 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    reversed = ""
+    for index in range(1, len(word) + 1):
+        reversed = reversed + word[len(word) - index]
+    return word == reversed
