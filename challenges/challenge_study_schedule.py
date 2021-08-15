a1845e738a758ad3469215d4f6a0eac794fc8025
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-15 20:46:45 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    if target_time is None:
+        return None
+
+    number = 0
+
+    for schedule in permanence_period:
+        if None in schedule:
+            return None
+        if target_time in range(schedule[0], schedule[1]) or target_time in schedule:
+            number += 1
+    return number
