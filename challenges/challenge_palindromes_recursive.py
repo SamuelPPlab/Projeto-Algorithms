dec90ef5c8142678670fd5f60d0afa7e7b690396
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-14 21:01:29 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        reversed = reverse(list[1:])
+        return reversed + list[0]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    return word == reverse(word)
