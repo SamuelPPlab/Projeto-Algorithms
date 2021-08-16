d1d48dc83d6ca2f7c51e41f43992ae6005931039
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-15 21:41:52 -0300

    bora FLAKE

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,12 +4,11 @@ def study_schedule(permanence_period, target_time):
         return None
 
     number = 0
-
+    
     for schedule in permanence_period:
+        schedule_ok = target_time in schedule
         if None in schedule:
             return None
-        if target_time in range(schedule[0], schedule[1]):
-            number += 1
-        if target_time in schedule:
+        if target_time in range(schedule[0], schedule[1]) or schedule_ok:
             number += 1
     return number
