a5635b124a8824bdf6826895254be57578899e06
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-15 22:29:04 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for number in nums:
+        if not isinstance(number, int) or number < 0:
+            return False
+        if nums.count(number) >= 2:
+            return number
+    return False
