80bd02383849429a0747b7b38a6a40962bd25cf8
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-15 21:27:22 -0300

    req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,10 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+        
+    wordcaracters = [char for char in word]
+    reversecaracters = wordcaracters[::-1]
+    if wordcaracters == reversecaracters:
+        return True
+    else:
+        return False
