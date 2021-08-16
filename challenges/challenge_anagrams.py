dbfa422dcb5cb890210d3959b32cee4210e1fb66
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-15 21:59:55 -0300

    req 3(em partes

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for caracter in first_string:
+        if second_string.find(caracter) !=  -1:
+            return True
+        else: 
+            return False
