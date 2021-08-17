72b303a6cd82f88c6ebdd98a773455740c922014
LAURO DE SA <itrafor@gmail.com>
2021-08-17 09:58:37 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    # entendi observando o codeReview #58 - carlos8V
+    students = 0
+    try:
+        for start_time, end_time in permanence_period:
+            if(start_time <= target_time <= end_time):
+                students += 1
+        return students
+    except TypeError:
+        return None
