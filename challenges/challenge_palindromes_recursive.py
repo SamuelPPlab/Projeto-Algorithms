57a375fbb5ad75bf8a4ebc3b68fa7c6cdaf00026
LAURO DE SA <itrafor@gmail.com>
2021-08-17 13:57:01 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    # entendi observando o codeReview #58 - carlos8V
+    try:
+        if word[low_index] != word[high_index]:
+            return False
+        elif low_index == high_index:
+            return True
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except IndexError:
+        return False
