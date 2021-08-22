6bc10339a45b8dccdab586c97c9a705eefb0c737
Lets <letsferreiratx@gmail.com>
2021-08-22 18:55:22 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,20 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+def duplicate(nums):
+    nums.sort()
+    for index in range(1, len(nums)):
+        if (nums[index] == nums[index-1]):
+            return nums[index]
+
+    return False
+
+
+def find_duplicate(nums):
+    if nums == []:
+        return False
+
+    for num in nums:
+        if isinstance(num, int) is False or num < 1:
+            return False
+
+    return duplicate(nums)
