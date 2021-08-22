94d9feae032f17d185d5032d6314614135475dfc
Lets <letsferreiratx@gmail.com>
2021-08-22 18:10:24 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
+def quicksort(lst):
+    if not lst:
+        return []
+    return (quicksort([x for x in lst[1:] if x <  lst[0]])
+            + [lst[0]] +
+            quicksort([x for x in lst[1:] if x >= lst[0]]))
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+
+    return quicksort(first_string) == quicksort(second_string)
+
+
+# https://www.delftstack.com/pt/howto/python/sort-list-alphabetically/
