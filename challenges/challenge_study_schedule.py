358e70069854a7cc1c3e310962ae3faeee6b1dc8
SamuelPPlab <samuelpp.s@hotmail.com>
2021-08-25 18:49:55 -0300

    problema de complexidade resolvido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,8 +3,8 @@ def study_schedule(permanence_period, target_time):
     number_of_online_students = 0
     for item in permanence_period:
         if (type(item[0]) is not int or
-        type(item[1]) is not int or
-        type(target_time) is not int):
+            type(item[1]) is not int or
+            type(target_time) is not int):
             return None
         if item[0] <= target_time and item[1] >= target_time:
             number_of_online_students += 1
