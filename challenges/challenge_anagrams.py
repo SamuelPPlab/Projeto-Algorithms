2e4a796d58b19883a48f74bd4312dc70373b1430
SamuelPPlab <samuelpp.s@hotmail.com>
2021-08-25 18:38:31 -0300

    todos os requisitos prontos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def sorting_algorithm(word):
-    #código baseado no dia 36.3 do course
-    #https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-
+    # código baseado no dia 36.3 do course
+    # https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-
     # 44ea-488d-a74d-216b1ac79b04/conteudos/
     # 118f97fe-3cc2-4468-a14c-ca8c341cadc2/algoritmos-de-ordenacao/a3ba0c1f-1835-4956-ba4a-42f376d3d555?use_case=side_bar
     letter_array = list(word)
@@ -12,6 +12,7 @@ def sorting_algorithm(word):
         letter_array[minimum], letter_array[i] = letter_array[i], letter_array[minimum]
 
     return letter_array
+
 
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
