383787cb5422ffac6f682accda37b40030181133
SamuelPPlab <samuelpp.s@hotmail.com>
2021-08-25 18:46:13 -0300

    flake 8

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,9 +4,9 @@ def is_palindrome_recursive(word, low_index, high_index):
     try:
         if len(word) == 1 or low_index == high_index:
             return True
-        elif word == '':
+        if word == '':
             return False
-        elif word[low_index] != word[high_index]:
+        if word[low_index] != word[high_index]:
             return False
         else:
             return is_palindrome_recursive(word, low_index +1, high_index -1)
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,9 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     number_of_online_students = 0
     for item in permanence_period:
-        if type(item[0]) is not int or type(item[1]) is not int or type(target_time) is not int:
+        if (type(item[0]) is not int or
+        type(item[1]) is not int or
+        type(target_time) is not int):
             return None
         if item[0] <= target_time and item[1] >= target_time:
             number_of_online_students += 1
