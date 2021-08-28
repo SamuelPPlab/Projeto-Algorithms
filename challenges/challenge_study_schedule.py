75e0310760a7972fb2c25a30d1c89f84953ced1c
vicmafe <victormateuslf@gmail.com>
2021-08-28 12:18:54 -0300

    study schedule done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    counter = 0
+
+    for element in permanence_period:
+        if type(element[0]) != int or type(element[1]) != int:
+            return None
+        if element[0] <= target_time <= element[1]:
+            counter += 1
+    return counter
