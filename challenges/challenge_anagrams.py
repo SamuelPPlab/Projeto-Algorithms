c03fc50e83754d628b576ab5d59eff735a6a848c
vicmafe <victormateuslf@gmail.com>
2021-08-28 18:36:38 -0300

    is anagram done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-    """start project!"""
+
+    first_string_list = [letter for letter in first_string]
+    second_string_list = [letter for letter in second_string]
+
+    for index, letter in enumerate(first_string_list):
+        letter_index = None
+
+        for current_index in range(index, len(second_string_list)):
+            if letter == second_string_list[current_index]:
+                letter_index = second_string_list[current_index]
+                break
+
+        if not letter_index:
+            break
+
+        second_string_list[index], second_string_list[current_index] = (
+            second_string_list[current_index],
+            second_string_list[index],
+        )
+
+    return first_string_list == second_string_list
