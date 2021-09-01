ee892cd4a0b1b53d73fafbed2498f7cd5daba2b3
vini-lima <vini20.henrique@gmail.com>
2021-09-01 08:03:20 -0300

    Requisito 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,13 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    
+
     students = 0
 
     for initial, final in permanence_period:
         if type(initial) != int or type(final) != int:
             return None
-          
+
         if initial <= target_time <= final:
             students += 1
 
