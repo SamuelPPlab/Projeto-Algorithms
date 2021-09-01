9aa472e7393719818334817bbed20865360c2546
flaviosoliver <flavsoliver@gmail.com>
2021-09-01 00:50:28 -0300

    req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    sort = sorted(nums)
+    if not nums:
+        return False
+    for index in range(1, len(sort)):
+        if not isinstance(nums[index], int) or nums[index] < 0:
+            return False
+        if sort[index] == sort[index - 1]:
+            return sort[index]
+    return False
