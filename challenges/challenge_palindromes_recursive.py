1cb5a53efdaf0a08085869591fd457284b53edc4
InkAurora <marthus2001@gmail.com>
2021-09-01 18:03:48 -0300

    All Done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word[1:high_index], 0, -1)
