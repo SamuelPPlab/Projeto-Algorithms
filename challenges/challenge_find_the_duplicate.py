d94bb88146f38b7f4dcd80ca6cc9787b5fe86df2
vini-lima <vini20.henrique@gmail.com>
2021-09-01 09:48:45 -0300

    Requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,42 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for number in nums:
+        if type(number) is str or number < 0:
+            return False
+
+    ordered_numbers = merge_sort(nums)
+    len_ordered_numbers = len(ordered_numbers)
+
+    for i in range(len_ordered_numbers - 1):
+        if ordered_numbers[i] == ordered_numbers[i + 1]:
+            return ordered_numbers[i]
+    return False
+
+
+# Algoritmo merge_sort do Course (Trybe)
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left, right = merge_sort(string[:mid]), merge_sort(string[mid:])
+    return merge(left, right, string)
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
