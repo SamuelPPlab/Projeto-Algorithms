51a116510b87ba6451d1370f32993980da71e384
vini-lima <vini20.henrique@gmail.com>
2021-09-01 09:58:46 -0300

    Requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+
+    middle = len(word)//2
+    initial = word[0:middle]
+    final = word[middle:len(word)]
+
+    if len(word) % 2 == 1:
+        final = final[1:]
+    if initial == final[::-1]:
+        return True
+    return False
