1f75707fb4c6cc43fc4323aa610098c939636473
flaviosoliver <flavsoliver@gmail.com>
2021-08-31 23:20:26 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,8 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    for index in first_string:
+        if second_string.find(index) != -1:
+            return True
+        else:
+            return False
