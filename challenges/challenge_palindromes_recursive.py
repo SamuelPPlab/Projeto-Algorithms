b859ae37d0f93382123b8a3b4881109c50949d77
josielvs <josielvs@gmail.com>
2021-09-03 01:22:25 -0300

    Requisito 2 - Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    try:
+        if low_index == high_index:
+            return True
+        elif word[high_index] != word[low_index]:
+            return False
+        else:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except IndexError:
+        return False
